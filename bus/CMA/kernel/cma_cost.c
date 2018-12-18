03bfd765aafeaafb8b1cef8af3b98262ce39e8ef
BuddyZhang1 <buddy.zhang@aliyun.com>
2018-12-18 14:24:36 +0800

    test code for CMA

---
+++ bus/CMA/kernel/cma_cost.c
@@ -0,0 +1,78 @@
+/*
+ * Request all CMA and doesn't release.
+ *
+ * (C) 2018.11.29 BiscuitOS <buddy.zhang@aliyun.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ */
+#include <stdio.h>
+#include <stdarg.h>
+#include <string.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <time.h>
+#include <sys/mman.h>
+#include <unistd.h>
+#include <sys/ioctl.h>
+
+#define CMA_MEM_VERSION         3
+#define CMEM_IOCTL_MAGIC        'm'
+#define CMEM_ALLOCATE           _IOW(CMEM_IOCTL_MAGIC, 1, unsigned int)
+#define CMEM_RELEASE            _IOW(CMEM_IOCTL_MAGIC, 2, unsigned int)
+ 
+struct cmamem_info {
+    unsigned int version;
+    unsigned int len;
+    unsigned int offset;
+    unsigned int mem_base;
+    unsigned int phy_base;
+};
+ 
+int main()
+{
+    int cmem_fd;
+    void *cmem_base;
+    unsigned int size;
+    struct cmamem_info region;
+    int i;
+
+    cmem_fd = open("/dev/cma_mem", O_RDWR, 0);
+    if (cmem_fd < 0) {
+        perror("Can't open /dev/CMA\n");
+        return -1;
+    }       
+    for (i = 0; i > -1; i++) {
+        memset(&region, 0x00, sizeof(struct cmamem_info));
+        region.version = CMA_MEM_VERSION;
+        region.len = 1 << 20;
+
+        if (ioctl(cmem_fd, CMEM_ALLOCATE, &region) < 0) {	
+            perror("PMEM_GET_TOTAL_SIZE failed\n");
+            return -1;
+        }
+        size = region.len;
+        cmem_base = mmap(0, size, PROT_READ | PROT_WRITE, 
+                                      MAP_SHARED, cmem_fd, region.phy_base);
+        printf("CMA[%d] Phyaddress: %#08x Virtual base: %#08x "
+               "Region.len: %#08x Offset: %#08x\n", i,
+                   (unsigned int)region.phy_base,
+                   (unsigned int)cmem_base, 
+                   (unsigned int)region.len, 
+                   (unsigned int)region.offset);
+        if (cmem_base == MAP_FAILED) {
+            cmem_base = 0;
+            close(cmem_fd);
+            cmem_fd = -1;
+            perror("mmap pmem error!\n");
+        }
+
+    }
+		
+    close(cmem_fd);
+    return 0;
+}
