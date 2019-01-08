61f6c5717c17b7bc5bcb85b173fba1c41445b9ef
BuddyZhang1 <buddy.zhang@aliyun.com>
2019-01-08 11:13:18 +0800

    DTS API: of_get_flat_dt_prop

---
+++ bus/DTS/API/of_get_flat_dt_prop/README.md
@@ -0,0 +1,35 @@
+of_get_flat_dt_root
+----------------------------------
+
+Context:
+
+* DTS Files: demo.dtsi
+
+* Driver Files: demo.c
+
+## Usage
+
+Copy DTS Files into `/arch/arm64/boot/dts`, and modify core specifical DTS 
+file as follow:
+
+```
+include "demo.dtsi"
+```
+
+Copy Driver Files into `/drivers/xxx/`, and modify Makefile on current 
+directory, as follow:
+
+```
+obj-y += demo.o
+```
+
+Then, compile driver and dts. Details :
+
+```
+make
+make dtbs
+```
+
+## Running
+
+Packing image and runing on the target board.
---
+++ bus/DTS/API/of_get_flat_dt_prop/dts.c
@@ -0,0 +1,87 @@
+/*
+ * DTS: 
+ *    of_get_flat_dt_root()
+ *    of_get_flat_dt_prop()
+ *
+ * (C) 2018.11.14 <buddy.zhang@aliyun.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ */
+/*
+ * Private DTS file: DTS_demo.dtsi
+ *
+ * / {
+ *        BiscuitOS-int = <0x10>;
+ *        BiscuitOS-name = "BiscuitOS";
+ * 
+ *        DTS_demo {
+ *                compatible = "DTS_demo, BiscuitOS";
+ *                status = "okay";
+ *        };
+ * };
+ *
+ * On Core dtsi:
+ *
+ * include "DTS_demo.dtsi"
+ */
+
+#include <linux/init.h>
+#include <linux/kernel.h>
+#include <linux/of_platform.h>
+#include <linux/of_fdt.h>
+
+/* define name for device and driver */
+#define DEV_NAME "DTS_demo"
+
+/* probe platform driver */
+static int DTS_demo_probe(struct platform_device *pdev)
+{
+    struct device_node *np = pdev->dev.of_node;
+    unsigned long dt_root;
+    unsigned int dt_int;
+    const char *dt_char;
+    
+    printk("DTS demo probe entence\n");
+
+    /* find the root node in the flat blob */
+    dt_root = of_get_flat_dt_root();
+
+    /* Obtain int property */
+    dt_int = of_get_flat_dt_prop(dt_root, "BiscuitOS-int", NULL);
+    /* Obtain char * property */
+    dt_char = of_get_flat_dt_prop(dt_root, "BiscuitOS-name", NULL);
+
+    if (dt_int > 0)
+        printk("BiscuitOS-int property: %#x\n", dt_int);
+
+    if (dt_char)
+        printk("BiscuitOS-name property: %s\n", dt_char);
+
+    return 0;
+}
+
+/* remove platform driver */
+static int DTS_demo_remove(struct platform_device *pdev)
+{
+    return 0;
+}
+
+static const struct of_device_id DTS_demo_of_match[] = {
+    { .compatible = "DTS_demo, BiscuitOS", },
+    { },
+};
+MODULE_DEVICE_TABLE(of, DTS_demo_of_match);
+
+/* platform driver information */
+static struct platform_driver DTS_demo_driver = {
+    .probe  = DTS_demo_probe,
+    .remove = DTS_demo_remove,
+    .driver = {
+        .owner = THIS_MODULE,
+        .name = DEV_NAME, /* Same as device name */
+        .of_match_table = DTS_demo_of_match,
+    },
+};
+module_platform_driver(DTS_demo_driver);
