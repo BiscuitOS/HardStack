f056e39eab2bf2942f7b9b5900e16798b93c50d3
BuddyZhang1 <buddy.zhang@aliyun.com>
2019-01-11 14:22:15 +0800

    DTS: of_get_phy_mode() obtain PHY mode from special device node

---
+++ bus/DTS/API/of_get_phy_mode/dts.c
@@ -0,0 +1,80 @@
+/*
+ * DTS: of_get_phy_mode
+ *
+ * const int of_get_phy_mode(struct device_node *np)
+ *
+ * (C) 2019.01.11 BuddyZhang1 <buddy.zhang@aliyun.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ */
+
+/*
+ * Private DTS file: DTS_demo.dtsi
+ *
+ * / {
+ *        DTS_demo {
+ *                compatible = "DTS_demo, BiscuitOS";
+ *                status = "okay";
+ *                phy-mode = "sgmii";
+ *        };
+ * };
+ *
+ * On Core dtsi:
+ *
+ * include "DTS_demo.dtsi"
+ */
+
+#include <linux/init.h>
+#include <linux/kernel.h>
+#include <linux/of_platform.h>
+#include <linux/of_net.h>
+#include <linux/phy.h>
+
+/* define name for device and driver */
+#define DEV_NAME "DTS_demo"
+
+/* probe platform driver */
+static int DTS_demo_probe(struct platform_device *pdev)
+{
+    struct device_node *np = pdev->dev.of_node;
+    int mode;
+    
+    printk("DTS demo probe entence\n");
+
+    /* Read PHY mode from DTS */
+    mode = of_get_phy_mode(np);
+    if (mode < 0)
+        return mode;
+
+    /* output mode string */
+    if (mode == PHY_INTERFACE_MODE_SGMII)
+        printk("PHY mode: sgmii\n");
+
+    return 0;
+}
+
+/* remove platform driver */
+static int DTS_demo_remove(struct platform_device *pdev)
+{
+    return 0;
+}
+
+static const struct of_device_id DTS_demo_of_match[] = {
+    { .compatible = "DTS_demo, BiscuitOS", },
+    { },
+};
+MODULE_DEVICE_TABLE(of, DTS_demo_of_match);
+
+/* platform driver information */
+static struct platform_driver DTS_demo_driver = {
+    .probe  = DTS_demo_probe,
+    .remove = DTS_demo_remove,
+    .driver = {
+        .owner = THIS_MODULE,
+        .name = DEV_NAME, /* Same as device name */
+        .of_match_table = DTS_demo_of_match,
+    },
+};
+module_platform_driver(DTS_demo_driver);
