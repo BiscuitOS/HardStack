eb977926fa73c087b8d453001685f5b3d9c79c44
BuddyZhang1 <buddy.zhang@aliyun.com>
2019-01-11 16:03:22 +0800

    DTS: of_get_mac_address() obtain MAC address from DT

---
+++ bus/DTS/API/of_get_mac_address/dts.c
@@ -0,0 +1,77 @@
+/*
+ * DTS: of_get_mac_address
+ *
+ * const void *of_get_mac_address(struct device_node *np)
+ *
+ * (C) 2019.01.11 BuddyZhang1 <buddy.zhang@aliyun.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ */
+
+/*
+ * Private DTS file: DTS_demo.dtsi
+ *
+ * / {
+ *        DTS_demo {
+ *                compatible = "DTS_demo, BiscuitOS";
+ *                status = "okay";
+ *                mac-address = [00 00 00 00 00 00];
+ *                local-mac-address = [00 0a 35 00 b4 c0];
+ *        };
+ * };
+ *
+ * On Core dtsi:
+ *
+ * include "DTS_demo.dtsi"
+ */
+
+#include <linux/init.h>
+#include <linux/kernel.h>
+#include <linux/of_platform.h>
+#include <linux/of_net.h>
+
+/* define name for device and driver */
+#define DEV_NAME "DTS_demo"
+
+/* probe platform driver */
+static int DTS_demo_probe(struct platform_device *pdev)
+{
+    struct device_node *np = pdev->dev.of_node;
+    const char *mac;
+    
+    printk("DTS demo probe entence\n");
+
+    /* obtain MAC address from DTS */
+    mac = of_get_mac_address(np);
+    if (mac)
+        printk("MAC Address: %02x %02x %02x %02x %02x %02x\n", 
+                 mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
+
+    return 0;
+}
+
+/* remove platform driver */
+static int DTS_demo_remove(struct platform_device *pdev)
+{
+    return 0;
+}
+
+static const struct of_device_id DTS_demo_of_match[] = {
+    { .compatible = "DTS_demo, BiscuitOS", },
+    { },
+};
+MODULE_DEVICE_TABLE(of, DTS_demo_of_match);
+
+/* platform driver information */
+static struct platform_driver DTS_demo_driver = {
+    .probe  = DTS_demo_probe,
+    .remove = DTS_demo_remove,
+    .driver = {
+        .owner = THIS_MODULE,
+        .name = DEV_NAME, /* Same as device name */
+        .of_match_table = DTS_demo_of_match,
+    },
+};
+module_platform_driver(DTS_demo_driver);
