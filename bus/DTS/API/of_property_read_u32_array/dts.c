89884ae4d54961722cf068991d027a25f828dfe6
BuddyZhang1 <buddy.zhang@aliyun.com>
2019-01-14 11:38:25 +0800

    DTS: of_property_read_u32_array()

---
+++ bus/DTS/API/of_property_read_u32_array/dts.c
@@ -0,0 +1,81 @@
+/*
+ * DTS: of_property_read_u32_array
+ *
+ * int of_property_read_u32_array(const struct device_node *np, 
+ *              const char *propname, u32 *out_value,
+ *              size_t sz)
+ *
+ * (C) 2019.01.11 BuddyZhang1 <buddy.zhang@aliyun.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ */
+
+/*
+ * Private DTS file: DTS_demo.dtsi
+ *
+ * / {
+ *        DTS_demo {
+ *                compatible = "DTS_demo, BiscuitOS";
+ *                status = "okay";
+ *                BiscuitOS-array  = <0x10 0x20 0x30 0x40 0x50>;
+ *        };
+ * };
+ *
+ * On Core dtsi:
+ *
+ * include "DTS_demo.dtsi"
+ */
+
+#include <linux/init.h>
+#include <linux/kernel.h>
+#include <linux/of_platform.h>
+
+/* define name for device and driver */
+#define DEV_NAME "DTS_demo"
+
+/* probe platform driver */
+static int DTS_demo_probe(struct platform_device *pdev)
+{
+    struct device_node *np = pdev->dev.of_node;
+    u32 array[5]; 
+    int ret;
+
+    printk("DTS demo probe entence.\n");
+
+    /* Read array from device_node */
+    ret = of_property_read_u32_array(np, "BiscuitOS-array", array, 5);
+    if (ret != 0) {
+        printk("Unable to read array from device node\n");
+        return -1;
+    }
+    printk("Arary: %#x %#x %#x %#x %#x\n", array[0], array[1], array[2],
+                       array[3], array[4]);
+
+    return 0;
+}
+
+/* remove platform driver */
+static int DTS_demo_remove(struct platform_device *pdev)
+{
+    return 0;
+}
+
+static const struct of_device_id DTS_demo_of_match[] = {
+    { .compatible = "DTS_demo, BiscuitOS", },
+    { },
+};
+MODULE_DEVICE_TABLE(of, DTS_demo_of_match);
+
+/* platform driver information */
+static struct platform_driver DTS_demo_driver = {
+    .probe  = DTS_demo_probe,
+    .remove = DTS_demo_remove,
+    .driver = {
+        .owner = THIS_MODULE,
+        .name = DEV_NAME, /* Same as device name */
+        .of_match_table = DTS_demo_of_match,
+    },
+};
+module_platform_driver(DTS_demo_driver);
