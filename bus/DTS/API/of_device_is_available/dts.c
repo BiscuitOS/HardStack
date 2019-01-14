273210f025a4a9360c22a685cb8c62fb17a60dd4
BuddyZhang1 <buddy.zhang@aliyun.com>
2019-01-14 14:02:29 +0800

    DTS: of_device_is_available()

---
+++ bus/DTS/API/of_device_is_available/dts.c
@@ -0,0 +1,73 @@
+/*
+ * DTS: of_device_is_avaiable
+ *
+ * int of_device_is_available(const struct device_node *device)
+ *
+ * (C) 2019.01.11 BuddyZhang1 <buddy.zhang@aliyun.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ */
+
+/*
+ * Private DTS file: DTS_demo.dtsi
+ *
+ * / {
+ *        DTS_demo {
+ *                compatible = "DTS_demo, BiscuitOS";
+ *                status = "okay";
+ *        };
+ * };
+ *
+ * On Core dtsi:
+ *
+ * include "DTS_demo.dtsi"
+ */
+
+#include <linux/init.h>
+#include <linux/kernel.h>
+#include <linux/of_platform.h>
+
+/* define name for device and driver */
+#define DEV_NAME "DTS_demo"
+
+/* probe platform driver */
+static int DTS_demo_probe(struct platform_device *pdev)
+{
+    struct device_node *np = pdev->dev.of_node;
+    struct device_node *npp;
+    const char *comp;
+
+    printk("DTS demo probe entence.\n");
+
+    if (of_device_is_available(np)) {
+        printk("%s device is available\n", np->name);
+    }
+
+    return 0;
+}
+
+/* remove platform driver */
+static int DTS_demo_remove(struct platform_device *pdev)
+{
+    return 0;
+}
+
+static const struct of_device_id DTS_demo_of_match[] = {
+    { .compatible = "DTS_demo, BiscuitOS", },
+    { },
+};
+MODULE_DEVICE_TABLE(of, DTS_demo_of_match);
+
+/* platform driver information */
+static struct platform_driver DTS_demo_driver = {
+    .probe  = DTS_demo_probe,
+    .remove = DTS_demo_remove,
+    .driver = {
+        .owner = THIS_MODULE,
+        .name = DEV_NAME, /* Same as device name */
+        .of_match_table = DTS_demo_of_match,
+    },
+};
+module_platform_driver(DTS_demo_driver);
