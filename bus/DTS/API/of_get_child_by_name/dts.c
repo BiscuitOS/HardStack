29898fcb84aaee4fa446e5f2aff83c66cf62a06b
BuddyZhang1 <buddy.zhang@aliyun.com>
2019-01-17 14:36:25 +0800

    DTS: of_get_child_by_name()

---
+++ bus/DTS/API/of_get_child_by_name/dts.c
@@ -0,0 +1,83 @@
+/*
+ * DTS: of_get_child_by_name
+ *
+ * struct device_node *of_get_child_by_name(const struct device_node *node,
+ *                                          const char *name)
+ *
+ * (C) 2019.01.11 BuddyZhang1 <buddy.zhang@aliyun.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ */
+
+/*
+ * Private DTS file: DTS_demo.dtsi
+ *
+ * / {
+ *        DTS_demo {
+ *                compatible = "DTS_demo, BiscuitOS";
+ *                status = "okay";
+ *
+ *                child0 {
+ *                         compatible = "Child0, BiscuitOS";
+ *                };
+ *
+ *                child1 {
+ *                         compatbile = "Child1, BiscuitOS";
+ *                };
+ *        };
+ * };
+ *
+ * On Core dtsi:
+ *
+ * include "DTS_demo.dtsi"
+ */
+
+#include <linux/init.h>
+#include <linux/kernel.h>
+#include <linux/of_platform.h>
+#include <linux/module.h>
+
+/* define name for device and driver */
+#define DEV_NAME "DTS_demo"
+
+/* probe platform driver */
+static int DTS_demo_probe(struct platform_device *pdev)
+{
+    struct device_node *np = pdev->dev.of_node;
+    struct device_node *child;
+  
+    printk("DTS probe entence...\n");
+
+    /* Get child node by name */
+    child = of_get_child_by_name(np, "child0");
+    if (child)
+        printk("\"%s\" child: \"%s\"\n", np->full_name, child->full_name);
+
+    return 0;
+}
+
+/* remove platform driver */
+static int DTS_demo_remove(struct platform_device *pdev)
+{
+    return 0;
+}
+
+static const struct of_device_id DTS_demo_of_match[] = {
+    { .compatible = "DTS_demo, BiscuitOS",  },
+    { },
+};
+MODULE_DEVICE_TABLE(of, DTS_demo_of_match);
+
+/* platform driver information */
+static struct platform_driver DTS_demo_driver = {
+    .probe  = DTS_demo_probe,
+    .remove = DTS_demo_remove,
+    .driver = {
+        .owner = THIS_MODULE,
+        .name = DEV_NAME, /* Same as device name */
+        .of_match_table = DTS_demo_of_match,
+    },
+};
+module_platform_driver(DTS_demo_driver);
