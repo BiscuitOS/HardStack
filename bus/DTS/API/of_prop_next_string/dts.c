fc70b8ddfaf909222d451c2ff096330b6e6663e1
BuddyZhang1 <buddy.zhang@aliyun.com>
2019-01-18 09:50:09 +0800

    DTS: of_prop_next_string()

---
+++ bus/DTS/API/of_prop_next_string/dts.c
@@ -0,0 +1,77 @@
+/*
+ * DTS: of_prop_next_string
+ *
+ * const char *of_prop_next_string(struct property *prop, const char *cur)
+ *
+ * (C) 2019.01.11 BuddyZhang1 <buddy.zhang@aliyun.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ */
+
+/*
+ * Private DTS file: DTS_demo.dtsi
+ *
+ * / {
+ *        DTS_demo {
+ *                compatible = "DTS_demo, BiscuitOS";
+ *                status = "okay";
+ *                BiscuitOS-strings = "uboot", "kernel", 
+ *                                    "rootfs", "BiscuitOS";
+ *        };
+ * };
+ *
+ * On Core dtsi:
+ *
+ * include "DTS_demo.dtsi"
+ */
+
+#include <linux/init.h>
+#include <linux/kernel.h>
+#include <linux/of_platform.h>
+#include <linux/module.h>
+
+/* define name for device and driver */
+#define DEV_NAME "DTS_demo"
+
+/* probe platform driver */
+static int DTS_demo_probe(struct platform_device *pdev)
+{
+    struct device_node *np = pdev->dev.of_node;
+    struct property *prop;
+    const __be32 *lane = NULL;
+  
+    printk("DTS probe entence...\n");
+   
+    prop = of_find_property(np, "BiscuitOS-strings", NULL);
+    for (lane = of_prop_next_string(prop, NULL); lane; 
+                lane = of_prop_next_string(prop, lane))
+        printk("String value: %s\n", lane);
+
+    return 0;
+}
+
+/* remove platform driver */
+static int DTS_demo_remove(struct platform_device *pdev)
+{
+    return 0;
+}
+
+static const struct of_device_id DTS_demo_of_match[] = {
+    { .compatible = "DTS_demo, BiscuitOS",  },
+    { },
+};
+MODULE_DEVICE_TABLE(of, DTS_demo_of_match);
+
+/* platform driver information */
+static struct platform_driver DTS_demo_driver = {
+    .probe  = DTS_demo_probe,
+    .remove = DTS_demo_remove,
+    .driver = {
+        .owner = THIS_MODULE,
+        .name = DEV_NAME, /* Same as device name */
+        .of_match_table = DTS_demo_of_match,
+    },
+};
+module_platform_driver(DTS_demo_driver);
