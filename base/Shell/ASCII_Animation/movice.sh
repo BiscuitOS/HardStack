8b358bf68c2c598ccd0deb7c108926968793b5c1
BuddyZhang1 <buddy.zhang@aliyun.com>
2019-01-24 11:18:21 +0800

    Movice to ASCII-animoation

--- base/Shell/ASCII_Animation/movice.sh
+++ base/Shell/ASCII_Animation/movice.sh
@@ -4,36 +4,136 @@
 # Argument list 
 # 
 
-DOWNLOAD_DIR=~/image2ascii
-INSTALL_DIR=/usr/local/share/
-TOOLS=${INSTALL_DIR}/image2ascii/bin/image2ascii
+ROOT=`pwd`
+COLOR=0
+INPUT_FILE=
+OUTPUT_DIR=
+FREQUEN=10
+WIDTH=280
+HEIGH=180
+NAME=ASCII
+TIME=1
+START=00:00:00
 
-while getopts ":x" opt
+help_func() {
+	echo "Welcome to Image to ASCII"
+	echo "./movice.sh [-rwh] <-i INPUT_FILE> <-o OUTPUT_DIR>" 
+	echo ""
+	echo "  -i <...>   : Input image file path"
+	echo "  -o <...>   : Output directory"
+	echo "  -r <...>   : Movie frequency"
+	echo "  -w <...>   : Movie width"
+	echo "  -h <...>   : Movie heigh"
+	echo "  -t <...>   : Movie time"
+	echo "  -s <...>   : Movie start time"
+	echo ""
+	echo "e.g."
+	echo "./movice.sh -i temp.mp4 -o temp"
+}
+
+while getopts ":i:o:t:s:r:w:h:" opt
 do
 	case $opt in
-		x)
-			PreInstall=1	
+		o)
+			OUTPUT_DIR=$OPTARG	
+		;;
+		i)
+			INPUT_FILE=$OPTARG
+		;;
+		r)
+			FREQUEN=$OPTARG
+		;;
+		w)
+			WIDTH=$OPTARG
+		;;
+		h)
+			HEIGH=$OPTARG
+		;;
+		t)
+			TIME=$OPTARG
+		;;
+		s)
+			START=$OPTARG
 		;;
 		?)
-			echo "??????"
+			help_func
 			exit 1;;
 	esac
 done
 
-if [ ! -d ${INSTALL_DIR}/image2ascii ]; then
-	echo "Download Image2ASCII tools...."
-	sudo apt-get install -y golang-go > /dev/null
-	sudo apt-get install -y ffmpeg > /dev/null
-	mkdir -p $DOWNLOAD_DIR
-	echo -e "#!/bin/bash\n export GOPATH=$DOWNLOAD_DIR" > ./1.sh
-	source ./1.sh
-	rm ./1.sh
-	go get github.com/qeesung/image2ascii
-	sudo mkdir -p $INSTALL_DIR
-	sudo cp -rf $DOWNLOAD_DIR ${INSTALL_DIR}/
-	rm -rf $DOWNLOAD_DIR
-	sudo ln -s ${TOOLS} /usr/bin/
-	echo "Install finish....."
+if [ ! -f /usr/bin/jp2a ]; then
+	sudo apt-get install -y jp2a ffmpeg
 fi
 
-image2ascii
+## check argument
+if [ -z $INPUT_FILE ]; then
+	echo "ERROR: need special input file.."
+	echo ""
+	help_func
+	exit 1
+else
+	if [ ! -f $INPUT_FILE ]; then
+		echo "Input file doesn't exist!"
+		exit 1
+	fi
+fi
+if [ -z $OUTPUT_DIR ]; then
+	echo "ERROR: need special output dir"
+        echo ""
+        help_func
+        exit 1
+else
+	if [ -d $ROOT/$OUTPUT_DIR ]; then
+		rm -rf $ROOT/$OUTPUT_DIR 
+	fi
+	mkdir -p $ROOT/$OUTPUT_DIR
+fi
+
+# Capture image from movie
+if [ -d ${ROOT}/.tmp ]; then
+	rm -rf ${ROOT}/.tmp
+fi
+mkdir -p ${ROOT}/.tmp
+
+ffmpeg -i $INPUT_FILE -f image2 -r $FREQUEN -ss $START -t $TIME -s ${WIDTH}X${HEIGH} ${ROOT}/.tmp/${NAME}1%5d.jpeg
+
+# Cover image to ASCII
+COUNT_IMAGE=`ls -l ${ROOT}/.tmp | grep "^-"|wc -l`
+INDEX=0
+
+while [ $INDEX -lt ${COUNT_IMAGE} ]
+do
+	INDEX=`expr ${INDEX} + 1`
+	SUB_NAME=`expr 100000 + $INDEX`
+	filename=${ROOT}/.tmp/${NAME}${SUB_NAME}.jpeg
+	outfile=$ROOT/$OUTPUT_DIR/${NAME}${SUB_NAME}.batB
+	jp2a $filename > $outfile
+done
+
+INDEX=0
+while true
+do
+	if [ $INDEX -ge ${COUNT_IMAGE} ]; then
+		INDEX=0
+	fi
+	INDEX=`expr ${INDEX} + 1`
+	SUB_NAME=`expr 100000 + $INDEX`
+	filename=$ROOT/$OUTPUT_DIR/${NAME}${SUB_NAME}.batB
+	cat $filename
+	sleep 0.06
+done
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
