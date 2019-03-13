d6bc10a38db0a13c9833c692153cf91f5dbff01a
BuddyZhang1 <buddy.zhang@aliyun.com>
2019-03-13 18:11:01 +0800

    MEMBLOCK: memblock_is_nomap()

---
+++ Memory-Allocator/Memblock-allocator/API/memblock_is_nomap/memblock.c
@@ -0,0 +1,59 @@
+/*
+ * memblock allocator
+ *
+ * (C) 2019.03.05 BuddyZhang1 <buddy.zhang@aliyun.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * MEMBLOCK
+ *
+ *                                         
+ *                                         struct memblock_region
+ *                       struct            +------+------+--------+------+
+ *                       memblock_type     |      |      |        |      |
+ *                       +----------+      | Reg0 | Reg1 | ...    | Regn |
+ *                       |          |      |      |      |        |      |
+ *                       | regions -|----->+------+------+--------+------+
+ *                       | cnt      |      [memblock_memory_init_regions]
+ *                       |          |
+ * struct           o--->+----------+
+ * memblock         |
+ * +-----------+    |
+ * |           |    |
+ * | memory   -|----o
+ * | reserved -|----o
+ * |           |    |                      struct memblock_region
+ * +-----------+    |    struct            +------+------+--------+------+
+ *                  |    memblock_type     |      |      |        |      |
+ *                  o--->+----------+      | Reg0 | Reg1 | ...    | Regn |
+ *                       |          |      |      |      |        |      |
+ *                       | regions -|----->+------+------+--------+------+
+ *                       | cnt      |      [memblock_reserved_init_regions]
+ *                       |          |
+ *                       +----------+
+ */
+
+#include <linux/init.h>
+#include <linux/kernel.h>
+#include <linux/memblock.h>
+
+int bs_debug = 0;
+
+#ifdef CONFIG_DEBUG_MEMBLOCK_IS_NOMAP
+int __init debug_memblock_is_nomap(void)
+{
+	struct memblock_region *reg;
+	bool state;
+
+	/* Check memblock regions is nomap */
+	state = memblock_is_nomap(&memblock.memory.regions[0]);
+	if (state)
+		pr_info("MEMBLOCK memory.regions[0] is nomap.\n");
+	else
+		pr_info("MEMBLOCK memory.regions[0] is not nomap.\n");
+
+	return 0;
+}
+#endif
