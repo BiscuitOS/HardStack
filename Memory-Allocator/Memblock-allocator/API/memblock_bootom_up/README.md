22dd8d6bf93a0927870097d4670f4011fddcab67
BuddyZhang1 <buddy.zhang@aliyun.com>
2019-03-14 08:44:13 +0800

    MEMBLOCK: memblock_bootom_up()

---
+++ Memory-Allocator/Memblock-allocator/API/memblock_bootom_up/README.md
@@ -0,0 +1,22 @@
+memblock_bootom_up [中文教程](https://biscuitos.github.io/blog/MMU-ARM32-MEMBLOCK-memblock_information/)
+--------------------------------------------
+
+* function: Find a valid memory from special area.
+
+
+## Usage
+
+* patch file
+
+```
+git am *.patch kernel_src/
+```
+
+* driver file
+
+```
+Device Driver--->
+    [*]BiscuitOS Driver--->
+        [*]Memblock allocator
+            [*]memblock_bootom_up()
+```
---
+++ Memory-Allocator/Memblock-allocator/API/memblock_bootom_up/memblock.c
@@ -0,0 +1,55 @@
+/*
+ * memblock allocator
+ *
+ * (C) 2019.03.05 BuddyZhang1 <buddy.zhang@aliyun.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * MEMBLOCK
+ *
+ *                                         
+ *                                         struct memblock_region
+ *                       struct            +------+------+--------+------+
+ *                       memblock_type     |      |      |        |      |
+ *                       +----------+      | Reg0 | Reg1 | ...    | Regn |
+ *                       |          |      |      |      |        |      |
+ *                       | regions -|----->+------+------+--------+------+
+ *                       | cnt      |      [memblock_memory_init_regions]
+ *                       |          |
+ * struct           o--->+----------+
+ * memblock         |
+ * +-----------+    |
+ * |           |    |
+ * | memory   -|----o
+ * | reserved -|----o
+ * |           |    |                      struct memblock_region
+ * +-----------+    |    struct            +------+------+--------+------+
+ *                  |    memblock_type     |      |      |        |      |
+ *                  o--->+----------+      | Reg0 | Reg1 | ...    | Regn |
+ *                       |          |      |      |      |        |      |
+ *                       | regions -|----->+------+------+--------+------+
+ *                       | cnt      |      [memblock_reserved_init_regions]
+ *                       |          |
+ *                       +----------+
+ */
+
+#include <linux/init.h>
+#include <linux/kernel.h>
+#include <linux/memblock.h>
+
+int bs_debug = 0;
+
+#ifdef CONFIG_DEBUG_MEMBLOCK_BOTTOM_UP
+int __init debug_memblock_bootom_up(void)
+{
+	bool state;
+
+	/* Obtian MEMBLOCK allocator direction */
+	state = memblock_bottom_up();
+	pr_info("MEMBLOCK direction: %s", state ? "bottom-up" : "top-down");
+
+	return 0;
+}
+#endif
