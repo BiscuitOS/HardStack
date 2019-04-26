aaa710d99551afeaac2043c71976268186d5fda4
BuddyZhang1 <buddy.zhang@aliyun.com>
2019-04-26 15:07:35 +0800

    list_splice

---
+++ Algorithem/list/bindirect-list/API/list_splice/list.c
@@ -0,0 +1,83 @@
+/*
+ * bindirect-list
+ *
+ * (C) 20179.04.25 <buddy.zhang@aliyun.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ */
+
+/*
+ * bidirect-list
+ *
+ * +-----------+<--o    +-----------+<--o    +-----------+<--o    +-----------+
+ * |           |   |    |           |   |    |           |   |    |           |
+ * |      prev |   o----| prev      |   o----| prev      |   o----| prev      |
+ * | list_head |        | list_head |        | list_head |        | list_head |
+ * |      next |---o    |      next |---o    |      next |---o    |      next |
+ * |           |   |    |           |   |    |           |   |    |           |
+ * +-----------+   o--->+-----------+   o--->+-----------+   o--->+-----------+
+ *
+ */
+
+#include <linux/kernel.h>
+#include <linux/init.h>
+
+/* header of list */
+#include <linux/list.h>
+
+/* private structure */
+struct node {
+    const char *name;
+    struct list_head list;
+};
+
+/* Initialize a group node structure */
+static struct node node0 = { .name = "BiscuitOS_node0", };
+static struct node node1 = { .name = "BiscuitOS_node1", };
+static struct node node2 = { .name = "BiscuitOS_node2", };
+static struct node node3 = { .name = "BiscuitOS_node3", };
+static struct node node4 = { .name = "BiscuitOS_node4", };
+static struct node node5 = { .name = "BiscuitOS_node5", };
+static struct node node6 = { .name = "BiscuitOS_node6", };
+
+/* Declaration and implement a bindirect-list */
+LIST_HEAD(BiscuitOS_list);
+LIST_HEAD(BiscuitOS_blist);
+
+static __init int bindirect_demo_init(void)
+{
+	struct node *np;
+
+	/* add a new entry on special entry */
+	list_add_tail(&node0.list, &BiscuitOS_list);
+	list_add_tail(&node1.list, &BiscuitOS_list);
+	list_add_tail(&node2.list, &BiscuitOS_list);
+	list_add_tail(&node3.list, &BiscuitOS_list);
+
+	list_add_tail(&node4.list, &BiscuitOS_blist);
+	list_add_tail(&node5.list, &BiscuitOS_blist);
+	list_add_tail(&node6.list, &BiscuitOS_blist);
+
+	printk("BiscuitOS_list:\n");
+	/* Traverser all node on bindirect-list */
+	list_for_each_entry(np, &BiscuitOS_list, list)
+		printk("%s\n", np->name);
+
+	printk("BiscuitOS_blist:\n");
+	/* Traverser all node on bindirect-list */
+	list_for_each_entry(np, &BiscuitOS_blist, list)
+		printk("%s\n", np->name);
+
+	/* join two list */
+	list_splice(&BiscuitOS_blist, &BiscuitOS_list);
+
+	printk("Splice BiscuitOS_list:\n");
+	/* Traverser all node on bindirect-list */
+	list_for_each_entry(np, &BiscuitOS_list, list)
+		printk("%s\n", np->name);
+
+	return 0;
+}
+device_initcall(bindirect_demo_init);
