44070401c21a12b76550dea65a1e176fc9860ce0
BuddyZhang1 <buddy.zhang@aliyun.com>
2019-05-05 15:51:52 +0800

    atomic_set

---
+++ Algorithem/atomic/API/atomic_set/README.md
@@ -0,0 +1,27 @@
+atomic_set [中文教程](https://biscuitos.github.io/blog/LIST_atomic_set/)
+----------------------------------
+
+Set up a atomic value.
+
+Context:
+
+* Driver Files: atomic.c
+
+## Usage
+
+Copy Driver Files into `/drivers/xxx/`, and modify Makefile on current 
+directory, as follow:
+
+```
+obj-$(CONFIG_XX) += atomic.o
+```
+
+Then, compile driver and dts. Details :
+
+```
+make
+```
+
+## Running
+
+Packing image and runing on the target board.
---
+++ Algorithem/atomic/API/atomic_set/atomic.c
@@ -0,0 +1,49 @@
+/*
+ * atomic
+ *
+ * (C) 2019.05.05 <buddy.zhang@aliyun.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ */
+
+/* Memory access
+ *
+ *
+ *      +----------+
+ *      |          |
+ *      | Register |                                         +--------+
+ *      |          |                                         |        |
+ *      +----------+                                         |        |
+ *            A                                              |        |
+ *            |                                              |        |
+ * +-----+    |      +----------+        +----------+        |        |
+ * |     |<---o      |          |        |          |        |        |
+ * | CPU |<--------->| L1 Cache |<------>| L2 Cache |<------>| Memory |
+ * |     |<---o      |          |        |          |        |        |
+ * +-----+    |      +----------+        +----------+        |        |
+ *            |                                              |        |
+ *            o--------------------------------------------->|        |
+ *                         volatile/atomic                   |        |
+ *                                                           |        |
+ *                                                           +--------+
+ */
+
+#include <linux/kernel.h>
+#include <linux/init.h>
+
+/* atomic_* */
+#include <asm/atomic.h>
+
+static atomic_t BiscuitOS_counter = ATOMIC_INIT(8);
+
+static __init int atomic_demo_init(void)
+{
+	atomic_set(&BiscuitOS_counter, 6);
+
+	printk("ATOMIC init: %d\n", atomic_read(&BiscuitOS_counter));
+
+	return 0;
+}
+device_initcall(atomic_demo_init);
