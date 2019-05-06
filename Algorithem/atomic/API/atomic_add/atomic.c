f4b3460d58e7aaff5439135b07ea9610b86ee346
BuddyZhang1 <buddy.zhang@aliyun.com>
2019-05-06 17:49:29 +0800

    atomic_add

---
+++ Algorithem/atomic/API/atomic_add/atomic.c
@@ -0,0 +1,70 @@
+/*
+ * atomic
+ *
+ * (C) 2019.05.05 <buddy.zhang@aliyun.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ */
+
+/* Memory access
+ *
+ *
+ *      +----------+
+ *      |          |
+ *      | Register |                                         +--------+
+ *      |          |                                         |        |
+ *      +----------+                                         |        |
+ *            A                                              |        |
+ *            |                                              |        |
+ * +-----+    |      +----------+        +----------+        |        |
+ * |     |<---o      |          |        |          |        |        |
+ * | CPU |<--------->| L1 Cache |<------>| L2 Cache |<------>| Memory |
+ * |     |<---o      |          |        |          |        |        |
+ * +-----+    |      +----------+        +----------+        |        |
+ *            |                                              |        |
+ *            o--------------------------------------------->|        |
+ *                         volatile/atomic                   |        |
+ *                                                           |        |
+ *                                                           +--------+
+ */
+
+/*
+ * atomic_add (ARMv7 Cotex-A9MP)
+ *
+ * static inline void atomic_add(int i, atomic_t *v)
+ * {
+ *         unsigned long tmp;
+ *         int result;
+ *
+ *         prefetchw(&v->counter);
+ *         __asm__ volatile ("\n\t"
+ *         "@ atomic_add\n\t"
+ * "1:      ldrex   %0, [%3]\n\t"        @ result, tmp115
+ * "        add     %0, %0, %4\n\t"      @ result,
+ * "        strex   %1, %0, [%3]\n\t"    @ tmp, result, tmp115
+ * "        teq     %1, #0\n\t"          @ tmp
+ * "        bne     1b"
+ *          : "=&r" (result), "=&r" (tmp), "+Qo" (v->counter)
+ *          : "r" (&v->counter), "Ir" (i)
+ *          : "cc");
+ * }
+ */
+
+#include <linux/kernel.h>
+#include <linux/init.h>
+
+static atomic_t BiscuitOS_counter = ATOMIC_INIT(8);
+
+/* atomic_* */
+static __init int atomic_demo_init(void)
+{
+	/* Atomic add */
+	atomic_add(1, &BiscuitOS_counter);
+
+	printk("Atomic: %d\n", atomic_read(&BiscuitOS_counter));
+
+	return 0;
+}
+device_initcall(atomic_demo_init);
