7f57c2a2cfe3f8085a66afae327c56535b9c1e28
BuddyZhang1 <buddy.zhang@aliyun.com>
2019-05-07 14:22:37 +0800

    atomic_dec_unless_positive

---
+++ Algorithem/atomic/API/atomic_dec_unless_positive/atomic.c
@@ -0,0 +1,60 @@
+/*
+ * atomic
+ *
+ * (C) 2019.05.05 <buddy.zhang@aliyun.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ */
+
+/* Memory access
+ *
+ *
+ *      +----------+
+ *      |          |
+ *      | Register |                                         +--------+
+ *      |          |                                         |        |
+ *      +----------+                                         |        |
+ *            A                                              |        |
+ *            |                                              |        |
+ * +-----+    |      +----------+        +----------+        |        |
+ * |     |<---o      |          |        |          |        |        |
+ * | CPU |<--------->| L1 Cache |<------>| L2 Cache |<------>| Memory |
+ * |     |<---o      |          |        |          |        |        |
+ * +-----+    |      +----------+        +----------+        |        |
+ *            |                                              |        |
+ *            o--------------------------------------------->|        |
+ *                         volatile/atomic                   |        |
+ *                                                           |        |
+ *                                                           +--------+
+ */
+
+#include <linux/kernel.h>
+#include <linux/init.h>
+
+static atomic_t BiscuitOS_counter = ATOMIC_INIT(2);
+
+/* atomic_* */
+static __init int atomic_demo_init(void)
+{
+	/* Legacy data */
+	printk("[0]Atomic: %d\n", atomic_read(&BiscuitOS_counter));
+
+	atomic_dec_unless_positive(&BiscuitOS_counter);
+	
+	/* New data */
+	printk("[1]Atomic: %d\n", atomic_read(&BiscuitOS_counter));
+
+	/* add > 0 */
+	atomic_sub(3, &BiscuitOS_counter);
+	printk("[2]Atomic: %d\n", atomic_read(&BiscuitOS_counter));
+
+	atomic_dec_unless_positive(&BiscuitOS_counter);
+
+	/* New data */
+	printk("[3]Atomic: %d\n", atomic_read(&BiscuitOS_counter));
+
+	return 0;
+}
+device_initcall(atomic_demo_init);
