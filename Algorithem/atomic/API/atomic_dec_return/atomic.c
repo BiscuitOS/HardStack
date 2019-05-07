cc14da3340dae64e1d8bc868cd89e8fbd46d5b47
BuddyZhang1 <buddy.zhang@aliyun.com>
2019-05-07 11:28:29 +0800

    atomic_dec_return

---
+++ Algorithem/atomic/API/atomic_dec_return/atomic.c
@@ -0,0 +1,50 @@
+/*
+ * atomic
+ *
+ * (C) 2019.05.05 <buddy.zhang@aliyun.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ */
+
+/* Memory access
+ *
+ *
+ *      +----------+
+ *      |          |
+ *      | Register |                                         +--------+
+ *      |          |                                         |        |
+ *      +----------+                                         |        |
+ *            A                                              |        |
+ *            |                                              |        |
+ * +-----+    |      +----------+        +----------+        |        |
+ * |     |<---o      |          |        |          |        |        |
+ * | CPU |<--------->| L1 Cache |<------>| L2 Cache |<------>| Memory |
+ * |     |<---o      |          |        |          |        |        |
+ * +-----+    |      +----------+        +----------+        |        |
+ *            |                                              |        |
+ *            o--------------------------------------------->|        |
+ *                         volatile/atomic                   |        |
+ *                                                           |        |
+ *                                                           +--------+
+ */
+
+#include <linux/kernel.h>
+#include <linux/init.h>
+
+static atomic_t BiscuitOS_counter = ATOMIC_INIT(8);
+
+/* atomic_* */
+static __init int atomic_demo_init(void)
+{
+	/* Atomic value before data  */
+	printk("[0]Atomic: oiginal-> %d\n", atomic_read(&BiscuitOS_counter));
+
+	/* Atomic value after data  */
+	printk("[1]Atomic: new->     %d\n", 
+				atomic_dec_return(&BiscuitOS_counter));
+
+	return 0;
+}
+device_initcall(atomic_demo_init);
