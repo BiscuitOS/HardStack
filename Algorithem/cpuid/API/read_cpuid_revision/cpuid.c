bf237cd8be35456fdcf2547a6b82a9ac98164730
BuddyZhang1 <buddy.zhang@aliyun.com>
2019-05-09 11:58:03 +0800

    read_cpuid_revision

---
+++ Algorithem/cpuid/API/read_cpuid_revision/cpuid.c
@@ -0,0 +1,26 @@
+/*
+ * cpuid
+ *
+ * (C) 2019.05.08 <buddy.zhang@aliyun.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ */
+
+#include <linux/kernel.h>
+#include <linux/init.h>
+
+#include <asm/cputype.h>
+
+static __init int cpuid_demo_init(void)
+{
+	unsigned int info;
+
+	info = read_cpuid_revision();
+
+	printk("CPU revision: %#x\n", info);	
+
+	return 0;
+}
+device_initcall(cpuid_demo_init);
