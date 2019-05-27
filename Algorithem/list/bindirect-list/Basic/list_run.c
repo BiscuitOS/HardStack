4ac07893f7941bfd09d508d9150f8fc03c363a98
BuddyZhang1 <buddy.zhang@aliyun.com>
2019-05-27 15:02:43 +0800

    list_head on application

---
+++ Algorithem/list/bindirect-list/Basic/list_run.c
@@ -0,0 +1,64 @@
+/*
+ * list Manual.
+ *
+ * (C) 2019.05.27 <buddy.zhang@aliyun.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ */
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * bidirect-list
+*
+ * +-----------+<--o    +-----------+<--o    +-----------+<--o    +-----------+
+ * |           |   |    |           |   |    |           |   |    |           |
+ * |      prev |   o----| prev      |   o----| prev      |   o----| prev      |
+ * | list_head |        | list_head |        | list_head |        | list_head |
+ * |      next |---o    |      next |---o    |      next |---o    |      next |
+ * |           |   |    |           |   |    |           |   |    |           |
+ * +-----------+   o--->+-----------+   o--->+-----------+   o--->+-----------+
+ *
+ */
+/* list */
+#include <list.h>
+
+/* private structure */
+struct node {
+	const char *name;
+	struct list_head list;
+};
+
+/* Initialize a group node structure */
+static struct node node0 = { .name = "BiscuitOS_node0", };
+static struct node node1 = { .name = "BiscuitOS_node1", };
+static struct node node2 = { .name = "BiscuitOS_node2", };
+static struct node node3 = { .name = "BiscuitOS_node3", };
+static struct node node4 = { .name = "BiscuitOS_node4", };
+static struct node node5 = { .name = "BiscuitOS_node5", };
+static struct node node6 = { .name = "BiscuitOS_node6", };
+
+/* Declaration and implement a bindirect-list */
+static LIST_HEAD(BiscuitOS_list);
+
+int main()
+{
+	struct node *np;
+
+	/* add a new entry on back */
+	list_add_tail(&node0.list, &BiscuitOS_list);
+	list_add_tail(&node1.list, &BiscuitOS_list);
+	list_add_tail(&node2.list, &BiscuitOS_list);
+	list_add_tail(&node3.list, &BiscuitOS_list);
+	list_add_tail(&node4.list, &BiscuitOS_list);
+	list_add_tail(&node5.list, &BiscuitOS_list);
+	list_add_tail(&node6.list, &BiscuitOS_list);
+
+	/* Traverser all node on bindirect-list */
+	list_for_each_entry(np, &BiscuitOS_list, list)
+		printf("%s\n", np->name);
+
+	return 0;
+}
