c4c4c56c206c6d00f9ba962f193e7c71b893e174
BuddyZhang1 <buddy.zhang@aliyun.com>
2019-06-04 10:22:13 +0800

    IDR: idr_remove

---
+++ Algorithem/IDR/API/idr_remove/README.md
@@ -0,0 +1,27 @@
+idr_remove [中文教程](https://biscuitos.github.io/blog/IDR_idr_remove/)
+----------------------------------
+
+Remove an ID from IDR.
+
+Context:
+
+* Driver Files: idr.c
+
+## Usage
+
+Copy Driver Files into `/drivers/xxx/`, and modify Makefile on current 
+directory, as follow:
+
+```
+obj-$(CONFIG_IDR_XX) += idr.o
+```
+
+Then, compile driver or dts. Details :
+
+```
+make
+```
+
+## Running
+
+Packing image and runing on the target board.
---
+++ Algorithem/IDR/API/idr_remove/idr.c
@@ -0,0 +1,74 @@
+/*
+ * IDR.
+ *
+ * (C) 2019.06.04 <buddy.zhang@aliyun.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ */
+
+#include <linux/kernel.h>
+#include <linux/init.h>
+#include <linux/mm.h>
+
+/* header of radix-tree */
+#include <linux/idr.h>
+
+/* private node */
+struct node {
+	const char *name;
+};
+
+/* Root of IDR */
+static DEFINE_IDR(BiscuitOS_idr);
+
+/* node set */
+static struct node node0 = { .name = "IDA" };
+static struct node node1 = { .name = "IDB" };
+static struct node node2 = { .name = "IDC" };
+static struct node node3 = { .name = "IDD" };
+static struct node node4 = { .name = "IDE" };
+
+/* ID array */
+#define IDR_ARRAY_SIZE	5
+static int idr_array[IDR_ARRAY_SIZE];
+
+static __init int idr_demo_init(void)
+{
+	struct node *np;
+	int id;
+
+	/* proload for idr_alloc */
+	idr_preload(GFP_KERNEL);
+
+	/* Allocate a id from IDR */
+	idr_array[0] = idr_alloc_cyclic(&BiscuitOS_idr, &node0, 1,
+							INT_MAX, GFP_ATOMIC);
+	idr_array[1] = idr_alloc_cyclic(&BiscuitOS_idr, &node1, 1,
+							INT_MAX, GFP_ATOMIC);
+	idr_array[2] = idr_alloc_cyclic(&BiscuitOS_idr, &node2, 1,
+							INT_MAX, GFP_ATOMIC);
+	idr_array[3] = idr_alloc_cyclic(&BiscuitOS_idr, &node3, 1,
+							INT_MAX, GFP_ATOMIC);
+	idr_array[4] = idr_alloc_cyclic(&BiscuitOS_idr, &node4, 1,
+							INT_MAX, GFP_ATOMIC);
+
+	/* Interate over all slots */
+	idr_for_each_entry(&BiscuitOS_idr, np, id)
+		printk("%s's ID %d\n", np->name, id);
+
+	/* Remove an ID from the IDR */
+	idr_remove(&BiscuitOS_idr, idr_array[2]);
+	printk("Remove ID: %d\n", idr_array[2]);
+
+	/* Interate over all slots */
+	idr_for_each_entry(&BiscuitOS_idr, np, id)
+		printk("%s's ID %d\n", np->name, id);
+
+	/* end preload section started with idr_preload() */
+	idr_preload_end();
+
+	return 0;
+}
+device_initcall(idr_demo_init);
