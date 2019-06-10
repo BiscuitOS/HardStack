e5155fbf663b5a03e8867403c6ec78f4acaf45ea
BuddyZhang1 <buddy.zhang@aliyun.com>
2019-06-11 07:42:15 +0800

    Bitmap: __bitmap_andnot

---
+++ Algorithem/bitmap/API/__bitmap_andnot/bitmap.c
@@ -0,0 +1,28 @@
+/*
+ * Bitmap.
+ *
+ * (C) 2019.06.10 <buddy.zhang@aliyun.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ */
+
+#include <linux/kernel.h>
+#include <linux/init.h>
+
+/* header of bitmap */
+#include <linux/bitmap.h>
+
+static __init int bitmap_demo_init(void)
+{
+	unsigned long dst;
+	unsigned long bitmap1 = 0xffff0000;
+	unsigned long bitmap2 = 0x000fffff;
+
+	__bitmap_andnot(&dst, &bitmap1, &bitmap2, 32);
+	printk("DST: %#lx\n", dst);
+
+	return 0;
+}
+device_initcall(bitmap_demo_init);
