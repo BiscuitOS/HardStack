69f597ae76186e34c8282c0c756cb744fa018ad3
BuddyZhang1 <buddy.zhang@aliyun.com>
2019-06-12 07:02:29 +0800

    Bitmap: __bitmap_clear

---
+++ Algorithem/bitmap/API/__bitmap_clear/README.md
@@ -0,0 +1,27 @@
+\_\_bitmap_clear [中文教程](https://biscuitos.github.io/blog/BITMAP___bitmap_clear/)
+----------------------------------
+
+Clear special bit on Bitmap.
+
+Context:
+
+* Driver Files: bitmap.c
+
+## Usage
+
+Copy Driver Files into `/drivers/xxx/`, and modify Makefile on current 
+directory, as follow:
+
+```
+obj-$(CONFIG_BITMAP_XX) += bitmap.o
+```
+
+Then, compile driver or dts. Details :
+
+```
+make
+```
+
+## Running
+
+Packing image and runing on the target board.
---
+++ Algorithem/bitmap/API/__bitmap_clear/bitmap.c
@@ -0,0 +1,28 @@
+/*
+ * Bitmap.
+ *
+ * (C) 2019.06.10 <buddy.zhang@aliyun.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ */
+
+#include <linux/kernel.h>
+#include <linux/init.h>
+
+/* header of bitmap */
+#include <linux/bitmap.h>
+
+static __init int bitmap_demo_init(void)
+{
+	unsigned long bitmap1 = 0xffff00f1;
+	unsigned long bitmap2 = bitmap1;
+
+	/* clear special bits */
+	__bitmap_clear(&bitmap1, 4, 4);
+	printk("%#lx clear 4 bit: %#lx\n", bitmap2, bitmap1);
+
+	return 0;
+}
+device_initcall(bitmap_demo_init);
