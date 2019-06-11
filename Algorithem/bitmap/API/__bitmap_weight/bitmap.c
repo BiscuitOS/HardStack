c19158f4ce10c7563a9290736f6fac2b4824e79c
BuddyZhang1 <buddy.zhang@aliyun.com>
2019-06-11 09:11:55 +0800

    Bitmap: __bitmap_weight

---
+++ Algorithem/bitmap/API/__bitmap_weight/bitmap.c
@@ -0,0 +1,26 @@
+/*
+ * Bitmap.
+ *
+ * (C) 2019.06.10 <buddy.zhang@aliyun.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ */
+
+#include <linux/kernel.h>
+#include <linux/init.h>
+
+/* header of bitmap */
+#include <linux/bitmap.h>
+
+static __init int bitmap_demo_init(void)
+{
+	unsigned long bitmap1 = 0xffff0001;
+
+	printk("%#lx contains the number of 1: %d\n", bitmap1,
+				__bitmap_weight(&bitmap1, 20));
+
+	return 0;
+}
+device_initcall(bitmap_demo_init);
