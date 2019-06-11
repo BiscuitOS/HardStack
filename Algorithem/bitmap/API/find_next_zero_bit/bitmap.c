62cb1f61f41a3e2866d40a58d336e76b13ae5cd2
BuddyZhang1 <buddy.zhang@aliyun.com>
2019-06-12 07:50:42 +0800

    Bitmap: find_next_zero_bit

---
+++ Algorithem/bitmap/API/find_next_zero_bit/bitmap.c
@@ -0,0 +1,28 @@
+/*
+ * Bitmap.
+ *
+ * (C) 2019.06.10 <buddy.zhang@aliyun.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ */
+
+#include <linux/kernel.h>
+#include <linux/init.h>
+
+/* header of bitmap */
+#include <linux/bitmap.h>
+
+static __init int bitmap_demo_init(void)
+{
+	unsigned long bitmap1 = 0xffff00f1;
+	unsigned int pos;
+
+	/* Find first zero bit position. */
+	pos = find_next_zero_bit(&bitmap1, 32, 0);
+	printk("Find %#lx first zero bit postion is: %d\n", bitmap1, pos);
+
+	return 0;
+}
+device_initcall(bitmap_demo_init);
