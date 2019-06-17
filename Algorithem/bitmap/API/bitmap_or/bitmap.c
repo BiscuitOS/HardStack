575633fc90c50f1a9d3cd8ea771b45fea65d8fe9
BuddyZhang1 <buddy.zhang@aliyun.com>
2019-06-17 12:14:45 +0800

    Bitmap: bitmap_or

---
+++ Algorithem/bitmap/API/bitmap_or/bitmap.c
@@ -0,0 +1,29 @@
+/*
+ * Bitmap.
+ *
+ * (C) 2019.06.10 <buddy.zhang@aliyun.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ */
+
+#include <linux/kernel.h>
+#include <linux/init.h>
+
+/* header of bitmap */
+#include <linux/bitmap.h>
+
+static __init int bitmap_demo_init(void)
+{
+	unsigned long bitmap0 = 0x2345;
+	unsigned long bitmap1 = 0xff;
+	unsigned long bitmap;
+
+	/* OR operation */
+	bitmap_or(&bitmap, &bitmap0, &bitmap1, 12);
+	printk("%#lx OR %#lx = %#lx\n", bitmap0, bitmap1, bitmap);
+
+	return 0;
+}
+device_initcall(bitmap_demo_init);
