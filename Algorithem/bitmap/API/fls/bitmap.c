8950b593842feae11ab6fa2b193b408d0fa659c2
BuddyZhang1 <buddy.zhang@aliyun.com>
2019-06-18 10:20:49 +0800

    Bitmap: fls

---
+++ Algorithem/bitmap/API/fls/bitmap.c
@@ -0,0 +1,26 @@
+/*
+ * Bitmap.
+ *
+ * (C) 2019.06.10 <buddy.zhang@aliyun.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ */
+
+#include <linux/kernel.h>
+#include <linux/init.h>
+
+/* header of bitmap */
+#include <linux/bitmap.h>
+
+static __init int bitmap_demo_init(void)
+{
+	unsigned long bitmap = 0xfff00;
+
+	/* Find last set bit */
+	printk("%#lx last set bit: %d\n", bitmap, fls(bitmap));
+
+	return 0;
+}
+device_initcall(bitmap_demo_init);
