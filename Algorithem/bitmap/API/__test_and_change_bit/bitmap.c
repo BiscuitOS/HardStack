7b73899136227c4e23f17c6f733869df8fadba82
BuddyZhang1 <buddy.zhang@aliyun.com>
2019-06-21 09:54:40 +0800

    Bitmap: __test_and_change_bit

---
+++ Algorithem/bitmap/API/__test_and_change_bit/bitmap.c
@@ -0,0 +1,28 @@
+/*
+ * Bitmap.
+ *
+ * (C) 2019.06.10 <buddy.zhang@aliyun.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ */
+
+#include <linux/kernel.h>
+#include <linux/init.h>
+
+/* header of bitmap */
+#include <linux/bitmap.h>
+
+static __init int bitmap_demo_init(void)
+{
+	unsigned long bit = 0xff;
+	int ret;
+
+	/* Change a bit and return its old value */
+	ret = __test_and_change_bit(1, &bit);
+	printk("%#lx before 1's bit is: %d\n", bit, ret);
+
+	return 0;
+}
+device_initcall(bitmap_demo_init);
