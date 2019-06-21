1ffda34741e384a4a0566d42644a5bb809c28cfc
BuddyZhang1 <buddy.zhang@aliyun.com>
2019-06-21 10:35:22 +0800

    Bitmap: ____atomic_set_bit

---
+++ Algorithem/bitmap/API/____atomic_set_bit/bitmap.c
@@ -0,0 +1,27 @@
+/*
+ * Bitmap.
+ *
+ * (C) 2019.06.10 <buddy.zhang@aliyun.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ */
+
+#include <linux/kernel.h>
+#include <linux/init.h>
+
+/* header of bitmap */
+#include <linux/bitmap.h>
+
+static __init int bitmap_demo_init(void)
+{
+	unsigned long bit = 0xf0;
+
+	/* Change a bit and return its old value */
+	____atomic_set_bit(1, &bit);
+	printk("bit: %#lx\n", bit);
+
+	return 0;
+}
+device_initcall(bitmap_demo_init);
