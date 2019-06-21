45b825bf736983b4da9fc95c641183e141f7dcb4
BuddyZhang1 <buddy.zhang@aliyun.com>
2019-06-21 13:49:28 +0800

    Bitmap: bitmap_clear

---
+++ Algorithem/bitmap/API/bitmap_clear/bitmap.c
@@ -0,0 +1,27 @@
+/*
+ * Bitmap.
+ *
+ * (C) 2019.06.10 <buddy.zhang@aliyun.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ */
+
+#include <linux/kernel.h>
+#include <linux/init.h>
+
+/* header of bitmap */
+#include <linux/bitmap.h>
+
+static __init int bitmap_demo_init(void)
+{
+	unsigned long bitmap = 0xf000fff;
+
+	/* Clear range bits on bitmap */
+	bitmap_clear(&bitmap, 4, 4);
+	printk("Bitmap: %#lx\n", bitmap);
+
+	return 0;
+}
+device_initcall(bitmap_demo_init);
