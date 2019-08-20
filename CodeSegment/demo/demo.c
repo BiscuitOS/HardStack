293628bb4a7865358ccfe63e5b3ccc70c85c358b
BuddyZhang1 <buddy.zhang@aliyun.com>
2019-08-20 08:28:24 +0800

    Demo

---
+++ CodeSegment/demo/demo.c
@@ -0,0 +1,17 @@
+/*
+ * BiscuitOS Application
+ *
+ * (C) 2019.08.16 BiscuitOS <buddy.zhang@aliyun.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ */
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	printf("Hello BiscuitOS!\n");
+	return 0;
+}
