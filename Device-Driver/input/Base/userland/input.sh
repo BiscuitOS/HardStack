614d58bc049ed2a7d31cd0bf066831d0ac47d640
BuddyZhang1 <buddy.zhang@aliyun.com>
2019-10-07 09:23:06 +0800

    input.sh

--- Device-Driver/input/Base/userland/input.sh
+++ Device-Driver/input/Base/userland/input.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-# Misc Device
+# Input Device
 #
 # (C) 2019.09.24 BiscuitOS <buddy.zhang@aliyun.com>
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License version 2 as
 # published by the Free Software Foundation.
-
-ls /dev/Misc_demo
