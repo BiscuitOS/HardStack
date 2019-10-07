b69a3a1a9582e3fd5e1d3bc36a8260b928731413
BuddyZhang1 <buddy.zhang@aliyun.com>
2019-10-07 09:19:53 +0800

    LDD: input device application

--- Device-Driver/input/Base/userland/input.c
+++ Device-Driver/input/Base/userland/input.c
@@ -1,7 +1,7 @@
 /*
  * Input Device Driver Application
  *
- * (C) 2019.09.24 <buddy.zhang@aliyun.com>
+ * (C) 2019.10.07 <buddy.zhang@aliyun.com>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 as
@@ -12,42 +12,117 @@
 #include <string.h>
 #include <dirent.h>
 #include <unistd.h>
+#include <fcntl.h>
+#include <linux/input.h>
 
-#define INPUT_SYS_PATH	"/sys/devices/platform/Input_demo.1/input/"
+#define INPUT_SYS_PATH	"/sys/devices/platform/Input_demo.1/input"
+/* Input direct on /dev/input/ */
+static char input_dir[128] = "/dev/input";
 
 int main()
 {
+	struct input_event ev;
 	struct dirent *ptr;
-	char events[128];
-	char *input_dir = NULL;
+	char events_dir[128];
+	int find = 0;
 	DIR *dir;
+	int fd, rvl;
 
+	/* 1st step
+	 * --> Search input direct which contains events name on sysfs.
+	 * --> Such path: /sys/devices/platform/Input_demo.1/input/inputx
+	 */
 	if ((dir = opendir(INPUT_SYS_PATH)) == NULL) {
 		printf("Open %s dirent error.\n", INPUT_SYS_PATH);
 		return -1;
 	}
 
+	/* Search INPUT_SYS_PATH/input direct */
 	while ((ptr = readdir(dir)) != NULL) {
-		if (strcmp(ptr->d_name, ".") == 0 ||
-		    strcmp(ptr->d_name, "..") == 0) {
-			continue;
-		}
-		/* Search INPUT_SYS_PATH/input direct */
 		if (ptr->d_type == DT_DIR) {
 			if (!strncmp(ptr->d_name, "input", strlen("input"))) {
-				/* Find special input direct */
-				input_dir=ptr->d_name;
 				/* Create full path name */
-				strcpy(events, INPUT_SYS_PATH);
-				strcat(events, "/");
-				strcat(events, input_dir);
-				printf("FULL: %s\n", events);
+				strcpy(events_dir, INPUT_SYS_PATH);
+				strcat(events_dir, "/");
+				strcat(events_dir, ptr->d_name);
+				find = 1;
+				break;
 			}
 		}
 	}
 
-	/* close */
+	/* close Current Dirent */
 	closedir(dir);
 
+	if (!find) {
+		printf("Can't find %s/input\n", INPUT_SYS_PATH);
+		return -1;
+	}
+	find = 0;
+
+	/* 2nd step
+	 * --> Search events name which points to /dev/input/eventx
+	 */
+	if ((dir = opendir(events_dir)) == NULL) {
+		printf("Open %s dirent error.\n", events_dir);
+		return -1;
+	}
+
+	/* Search INPUT_SYS_PATH/input direct */
+	while ((ptr = readdir(dir)) != NULL) {
+		if (ptr->d_type == DT_DIR) {
+			if (!strncmp(ptr->d_name, "event", strlen("event"))) {
+				strcpy(events_dir, ptr->d_name);
+				find = 1;
+				break;
+			}
+		}
+	}
+
+	/* close Current Dirent */
+	closedir(dir);
+
+	if (!find) {
+		printf("Can't find events direct\n");
+		return -1;
+	}
+
+	/* 3th step
+	 * --> Open device node on /dev/input/eventx
+	 */
+	strcat(input_dir, "/");
+	strcat(input_dir, events_dir);
+
+	/* 4th step
+	 * --> Read event from /dev/input/eventx
+	 */
+	fd = open(input_dir, O_RDWR);
+	if (fd < 0) {
+		printf("Unable to open %s\n", input_dir);
+		return -1;
+	}
+	printf("Open Input Events: %s\n", input_dir);
+	
+	/* Read input events */
+	while (1) {
+		memset(&ev, 0, sizeof(struct input_event));
+		/* read events */
+		rvl = read(fd, &ev, sizeof(struct input_event));
+		if (rvl != sizeof(struct input_event)) {
+			printf("Can't read event frame\n");
+			goto err_event;
+		}
+		/* output event information */
+		printf("------------------------\n");
+		printf("type:   %u\n", ev.type);
+		printf("code:   %u\n", ev.code);
+		printf("value:  %u\n", ev.value);
+	}
+
+	close(fd);
 	return 0;
+
+err_event:
+	close(fd);
+	return rvl;
 }
