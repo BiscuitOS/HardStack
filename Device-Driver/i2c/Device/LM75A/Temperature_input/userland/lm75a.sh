aa92c0331eb47c340dc2f55029a03ba4a50e1d9f
BuddyZhang1 <buddy.d.zhang@gmail.com>
2019-11-06 15:44:31 +0800

    LDD: lm75a application

---
+++ Device-Driver/i2c/Device/LM75A/Temperature_input/userland/lm75a.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# LM75A Device Driver Application
+#
+# (C) 2019.09.24 BiscuitOS <buddy.zhang@aliyun.com>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 2 as
+# published by the Free Software Foundation.
+
+ls /dev/input/eventx
