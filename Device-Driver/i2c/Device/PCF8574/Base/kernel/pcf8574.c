0d52f291567b5e4cbeb82879cd1078e11a4f6005
BuddyZhang1 <buddy.d.zhang@gmail.com>
2019-11-09 13:24:12 +0800

    LDD: PCF8574AT 8-bit I/O expander

--- Device-Driver/i2c/Device/PCF8574/Base/kernel/pcf8574.c
+++ Device-Driver/i2c/Device/PCF8574/Base/kernel/pcf8574.c
@@ -18,6 +18,7 @@
 #include <linux/platform_device.h>
 #include <linux/of_platform.h>
 #include <linux/of_irq.h>
+#include <linux/of_gpio.h>
 #include <linux/interrupt.h>
 
 /* pcf8574 on DTS
@@ -25,11 +26,10 @@
  * arch/arm/boot/dts/bcm2711-rpi-4-b.dts
  *
  * &i2c1 {
- *        pcf8574@50 {
+ *        pcf8574@38 {
  *               compatible = "BiscuitOS,pcf8574";
  *               reg = <0x38>;
- *               interrupts-parent = <&gpio>;
- *               interrupts = <&gpio 58 IRQ_TYPE_LEVEL_HIGH>;
+ *               BD-gpio = <&gpio 25 GPIO_ACTIVE_HIGH>;
  *        };
  * };
  *
@@ -49,7 +49,7 @@
  * (GPIO_GEN3) GPIO22 | 15   16 | GPIO23 (GPIO_GEN4)
  *               3V3  | 17   18 | GPIO24 (GPIO_GEN5)
  *  (SPI_MOSI) GPIO10 | 19   20 | GND
- *   (SPI_MISO) GPIO9 | 21   22 | GPIO25 (GPIO_GEN6)
+ *   (SPI_MISO) GPIO9 | 21   22 | GPIO25 (GPIO_GEN6) <------ Interrupt
  *  (SPI_SCLK) GPIO11 | 23   24 | GPIO8  (SPI_CE0_N)
  *                GND | 25   26 | GPIO7  (SPI_CE1_N)
  *              ID_SD | 27   28 | ID_SC
@@ -79,9 +79,15 @@
 #define GPIO8			0x80
 #define GPIO_ALL		0xFF
 
+#define __unused		__attribute__((unused))
+
 struct pcf8574_priv
 {
+	struct i2c_client *client;
+	struct work_struct wq;
 	int irq;
+	int gpio;
+	u8  data; /* priv gpio state */
 };
 
 /* Read
@@ -107,7 +113,7 @@ struct pcf8574_priv
  *                                            K
  *
  */
-static int pcf8574a_read(struct i2c_client *client, 
+static int __unused pcf8574a_read(struct i2c_client *client, 
 				unsigned char offset, unsigned char *buf) 
 {
 	struct i2c_msg msgs;
@@ -142,8 +148,8 @@ static int pcf8574a_read(struct i2c_client *client,
  *    B           B W K   B           B K
  *
  */
-static int pcf8574a_write(struct i2c_client *client, unsigned char offset,
-							unsigned char data)
+static int __unused pcf8574a_write(struct i2c_client *client, 
+				unsigned char offset, unsigned char data)
 {
 	struct i2c_msg msgs;
 	unsigned char tmp[2];
@@ -162,15 +168,25 @@ static int pcf8574a_write(struct i2c_client *client, unsigned char offset,
 	return ret;
 }
 
+static void wq_isr(struct work_struct *work)
+{
+	struct pcf8574_priv *priv;
+	unsigned char buf[2];
+
+	priv = container_of(work, struct pcf8574_priv, wq);
+	/* Read GPIO Status */
+	memset(buf, 0, 2);
+	pcf8574a_read(priv->client, GPIO_ALL, buf);
+	printk("GPIO Status: %#hhx\n", buf[0]);
+}
+
 /* IRQ handler */
 static irqreturn_t pcf8574_isr(int irq, void *dev_id)
 {
-	struct i2c_client *client = (struct i2c_client *)dev_id;
-	char buf[2];
+	struct pcf8574_priv *priv = (struct pcf8574_priv *)dev_id;
 	
-	/* Read GPIO status */
-	pcf8574a_read(client, GPIO_ALL, buf);
-	printk("GPIO STATUS: %#hhx\n", buf[0]);
+	/* Low speed deal with on work queue */
+	schedule_work(&priv->wq);
 
 	return IRQ_HANDLED;
 }
@@ -192,24 +208,43 @@ static int pcf8574_probe(struct i2c_client *client,
 	}
 	
 	/* Get IRQ number from DTB */
-	priv->irq = of_irq_get(np, 0);
+	priv->gpio = of_get_named_gpio(np, "BD-gpio", 0);
+	if (priv->gpio < 0) {
+		printk("Error: unable to get GPIO from DTS\n");
+		ret = -EINVAL;
+		goto err_gpio;
+	}
+	gpio_direction_input(priv->gpio);
+	priv->irq = gpio_to_irq(priv->gpio);
+	if (priv->irq < 0) {
+		printk("Error: unable to get IRQ\n");
+		ret = -EINVAL;
+		goto err_gpio;
+	}
+
+	priv->client = client;
+	i2c_set_clientdata(client, priv);
+	/* setup GPIO3 as down */
+	pcf8574a_write(client, GPIO_ALL, GPIO3);
+	/* Initialize workqueue */
+	INIT_WORK(&priv->wq, wq_isr);
+	mdelay(100);
 
 	/* Require IRQ */
 	ret = request_irq(priv->irq, pcf8574_isr,
-				IRQF_TRIGGER_FALLING, DEV_NAME, client);
+				IRQF_TRIGGER_RISING, 
+				DEV_NAME, priv);
 	if (ret < 0) {
 		printk("Can't request IRQ %d\n", priv->irq);
 		return -EINVAL;
-		goto err_irq;
+		goto err_gpio;
 	}
 
-	/* Pull up GPIO4 and GPIO8, other Pull down */
-	pcf8574a_write(client, GPIO_ALL, GPIO4 | GPIO8);
+	printk("GPIO-Export IRQ: %d\n", priv->irq);
 
-	dev_set_drvdata(&client->dev, priv);
 	return 0;
 
-err_irq:
+err_gpio:
 	kfree(priv);
 err_alloc:
 	return ret;
@@ -221,9 +256,9 @@ static int pcf8574_remove(struct i2c_client *client)
 	struct pcf8574_priv *priv = dev_get_drvdata(&client->dev);
 
 	/* Free IRQ */
-	free_irq(priv->irq, NULL);
-
+	free_irq(priv->irq, priv);
 	kfree(priv);
+	i2c_set_clientdata(client, NULL);
 
 	return 0;
 }
