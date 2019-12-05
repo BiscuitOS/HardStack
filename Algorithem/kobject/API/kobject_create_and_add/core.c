a8d060b4dd6b8df1af13ba35213fe6df015f9ea9
BuddyZhang1 <buddy.d.zhang@gmail.com>
2019-12-05 19:32:05 +0800

    kobject

--- Algorithem/kobject/API/kobject_create_and_add/core.c
+++ Algorithem/kobject/API/kobject_create_and_add/core.c
@@ -12,15 +12,36 @@
 #include <linux/kernel.h>
 #include <linux/module.h>
 #include <linux/kobject.h>
+#include <linux/rbtree.h>
+#include <linux/rbtree_augmented.h>
 
 static struct kobject *kobj;
+extern struct kernfs_node *sysfs_root_kn;
+
+#define rb_to_kn(X) rb_entry((X), struct kernfs_node, rb)
 
 /* Module initialize entry */
 static int __init Demo_init(void)
 {
+	struct kernfs_node *kn, *pos;
+	struct rb_root *rbroot;
+	struct rb_node *np;
+
 	kobj = kobject_create_and_add("BiscuitOS", NULL);
 	if (!kobj)
 		return -ENOMEM;
+
+	kn = kobj->sd;
+	printk("Kobj: %s\n", kobject_name(kobj));
+
+	/* parent rbtree */
+	rbroot = &kn->parent->dir.children;
+	/* traverser all knode on rbtree */
+	for (np = rb_first_postorder(rbroot); np;
+				np = rb_next_postorder(np)) {
+		pos = rb_to_kn(np);
+		printk("NAME: %s\n", pos->name);
+	}
 
 	return 0;
 }
