bf5e3f34de285a9deb403a6de304a86a2a823be0
BuddyZhang1 <buddy.d.zhang@gmail.com>
2019-12-06 11:43:09 +0800

    vfs: get_fs_type()

---
+++ fs/vfs/API/get_fs_type/core.c
@@ -0,0 +1,41 @@
+/*
+ * BiscuitOS
+ *
+ * (C) 2019.10.01 BuddyZhang1 <buddy.zhang@aliyun.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ */
+
+#include <linux/init.h>
+#include <linux/kernel.h>
+#include <linux/module.h>
+#include <linux/fs.h>
+
+/* Module initialize entry */
+static int __init Demo_init(void)
+{
+	struct file_system_type *type;
+
+	/* get filesystem by name */
+	type = get_fs_type("rootfs");
+	if (!type)
+		panic("Can't find rootfs type");
+
+	printk("Filesystem: %s\n", type->name);
+
+	return 0;
+}
+
+/* Module exit entry */
+static void __exit Demo_exit(void)
+{
+}
+
+module_init(Demo_init);
+module_exit(Demo_exit);
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("BiscuitOS <buddy.zhang@aliyun.com>");
+MODULE_DESCRIPTION("BiscuitOS");
