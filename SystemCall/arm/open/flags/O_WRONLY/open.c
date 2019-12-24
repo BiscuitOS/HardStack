5e08ff89785dc41df3caee344d15f0a0269ead12
BuddyZhang1 <buddy.d.zhang@gmail.com>
2019-12-24 14:05:06 +0800

    Open: O_WRONALY

---
+++ SystemCall/arm/open/flags/O_WRONLY/open.c
@@ -0,0 +1,51 @@
+/*
+ * open: O_WRONLY
+ *
+ * (C) 2019.12.24 BuddyZhang1 <buddy.zhang@aliyun.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <string.h>
+#include <sys/stat.h>
+
+int main()
+{
+	char buffer[20] = "BiscuitOS";
+	int fd, ret;
+
+	/* open: Touch /etc/BiscuitOS.cfg before */
+	fd = open("/etc/BiscuitOS.cfg", O_WRONLY);
+	if (fd < 0) {
+		perror("/etc/BiscuitOS.cfg");
+		return -1;
+	}
+	/* write operation */
+	ret = write(fd, buffer, 9);
+	if (ret < 0) {
+		perror("Write failed");
+		close(fd);
+		return -1;
+	}
+
+	/* Force read with O_WRONLY */
+	memset(buffer, 0, 20);
+	ret = read(fd, buffer, 10);
+	if (ret < 0) {
+		perror("Read failed");
+		close(fd);
+		return -1;
+	}
+	buffer[10] = '\0';
+	printf("Read contents: %s\n", buffer);
+
+	/* close */
+	close(fd);
+
+	return 0;
+}
