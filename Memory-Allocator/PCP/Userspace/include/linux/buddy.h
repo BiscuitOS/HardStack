7539979f55c009a154fe285312d43b7f99df49cf
BuddyZhang1 <buddy.d.zhang@gmail.com>
2020-02-05 15:16:01 +0800

    PCP: Per CPU Page allocator

---
+++ Memory-Allocator/PCP/Userspace/include/linux/buddy.h
@@ -0,0 +1,145 @@
+#ifndef _BISCUITOS_BUDDY_H
+#define _BISCUITOS_BUDDY_H
+
+#include "linux/list.h"
+
+#define PAGE_SHIFT	12 /* 4KByte Page */
+#define PAGE_SIZE	(1 << PAGE_SHIFT)
+#define PAGE_MASK	(~((1 << PAGE_SHIFT) - 1))
+#define PFN_ALIGN(x)	(((unsigned long)(x) + (PAGE_SIZE - 1)) & PAGE_MASK)
+#define PFN_UP(x)	(((x) + PAGE_SIZE-1) >> PAGE_SHIFT)
+#define PFN_DOWN(x)	((x) >> PAGE_SHIFT)
+#define PFN_PHYS(x)	((phys_addr_t)(x) << PAGE_SHIFT)
+#define PHYS_PFN(x)	((unsigned long)(x) >> PAGE_SHIFT)
+
+/* Configuration Memory Region */
+#define MEMORY_SIZE	CONFIG_MEMORY_SIZE
+/* Configuration Basic Physical Address */
+#define PHYS_OFFSET	CONFIG_PHYS_BASE
+
+#define PFN_OFFSET	PHYS_PFN(PHYS_OFFSET)
+
+/* Buddy Allocator Order */
+#define MAX_ORDER	11
+
+/* GFP flag combinations */
+#define GFP_KERNEL	0x10000000
+
+typedef unsigned long phys_addr_t;
+typedef unsigned long gfp_t;
+
+struct free_area {
+	struct list_head free_list[1];
+	unsigned long nr_free;
+};
+
+struct zone {
+	/* free areas of different sizes */
+	struct free_area free_area[MAX_ORDER];
+};
+
+struct page {
+	unsigned int page_type;
+	unsigned long private;
+	struct list_head lru;
+};
+
+/* PFN and PHYS */
+#define pfn_to_page(pfn)	(mem_map + ((pfn) - PFN_OFFSET))
+#define page_to_pfn(page)	((unsigned long)((page) - mem_map) + \
+							PFN_OFFSET)
+#define pfn_valid_within(pfn)	(1)
+
+/* Physical and Virtual */
+extern unsigned char *memory;
+static inline phys_addr_t virt_to_phys(const volatile void *x)
+{
+	return ((unsigned long)(x) - (unsigned long)memory) + 
+							PHYS_OFFSET;
+}
+
+static inline void *phys_to_virt(phys_addr_t x)
+{
+	return (void *)((x - PHYS_OFFSET) + (unsigned long)memory);
+}
+
+#define min_t(type, x, y)	({		\
+		type __min1 = (x);		\
+		type __min2 = (y);		\
+		__min1 < __min2 ? __min1 : __min2; })
+
+static inline unsigned long __ffs(unsigned long word)
+{
+	return __builtin_ctzl(word);
+}
+
+#define page_private(page)		((page)->private)
+#define set_page_private(page, v)		((page)->private = (v))
+
+static inline unsigned int page_order(struct page *page)
+{
+	/* PageBuddy() Must be checked by the caller */
+	return page_private(page);
+}
+
+/*
+ * For pages that are never mapped to userspace (and aren't PageSlab),
+ * page_type may by used. Because it is initialised to -1, we invert the
+ * sense of the bit, so __SetPageFoo *clears* the bit used for PageFoo,
+ * and __ClearPageFoo *sets* the bit used for PageFoo. We reserve a few
+ * high and low bits so that and underflow or overflow of page_mapcount()
+ * won't be mistaken for a page type value.
+ */
+
+#define PAGE_TYPE_BASE		0xf0000000
+/* Reserve	0x0000007f to catch underflows of page_mapcount */
+#define PG_buddy		0x00000080
+
+#define PageType(page, flag)				\
+	((page->page_type & (PAGE_TYPE_BASE | flag)) == PAGE_TYPE_BASE)
+
+/* PageBuddy() indicates that the page is free and in the buddy system
+ * (see mm/page_alloc.c).
+ */
+static inline int PageBuddy(struct page *page)
+{
+	return PageType(page, PG_buddy);
+}
+static inline void __SetPageBuddy(struct page *page)
+{
+	page->page_type &= ~PG_buddy;
+}
+static inline void __ClearPageBuddy(struct page *page)
+{
+	page->page_type |= PG_buddy;
+}
+
+static inline void set_page_order(struct page *page, unsigned int order)
+{
+	set_page_private(page, order);
+	__SetPageBuddy(page);
+}
+
+static inline void rmv_page_order(struct page *page)
+{
+	__ClearPageBuddy(page);
+	set_page_private(page, 0);
+}
+
+#define likely(x)	__builtin_expect(!!(x), 1)
+#define unlikely(x)	__builtin_expect(!!(x), 0)
+
+extern struct zone BiscuitOS_zone;
+static inline struct zone *page_zone(const struct page *page)
+{
+	return &BiscuitOS_zone;
+}
+
+extern struct page *mem_map;
+extern int memory_init(void);
+extern void memory_exit(void);
+/* Huge page sizes are variable */
+extern unsigned int pageblock_order;
+extern void __free_pages(struct page *page, unsigned int order);
+extern struct page *__alloc_pages(gfp_t gfp_mask, unsigned int order);
+#endif
---
+++ Memory-Allocator/PCP/Userspace/mm/buddy.c
@@ -0,0 +1,389 @@
+/*
+ * Buddy Allocator
+ *
+ * (C) 2020.02.02 BuddyZhang1 <buddy.zhang@aliyun.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <malloc.h>
+
+#include "linux/buddy.h"
+
+/* nr_pages for memory */
+unsigned long nr_pages;
+/* Simulate memory */
+unsigned char *memory;
+/* mem_map array */
+struct page *mem_map;
+/* Huge page sizes are variable */
+unsigned int pageblock_order = 10;
+/* Emulate Zone */
+struct zone BiscuitOS_zone;
+
+/*
+ * Locate the struct page for both the matching buddy in our
+ * pair (buddy1) and the combined O(n+1) page they form (page).
+ *
+ * 1) Any buddy B1 will have an order O twin B2 which satisfies
+ *    the following equation:
+ *
+ *    For example, if the starting buddy (buddy2) is #8 its order
+ *    1 buddy is #10:
+ *
+ *      B2 = 0 ^ (1 << 1) = 0 ^ 2 = 0000B ^ 0010B = 0010B = 2
+ *      B2 = 2 ^ (1 << 1) = 2 ^ 2 = 0010B ^ 0010B = 0000B = 0
+ *      B2 = 4 ^ (1 << 1) = 4 ^ 2 = 0100B ^ 0010B = 0110B = 6
+ *      B2 = 6 ^ (1 << 1) = 6 ^ 2 = 0110B ^ 0010B = 0100B = 4
+ *      B2 = 8 ^ (1 << 1) = 8 ^ 2 = 1000B ^ 0010B = 1010B = A
+ *      B2 = A ^ (1 << 1) = A ^ 2 = 1010B ^ 0010B = 1000B = 8
+ *      B2 = C ^ (1 << 1) = C ^ 2 = 1100B ^ 0010B = 1110B = E
+ *      B2 = E ^ (1 << 1) = E ^ 2 = 1110B ^ 0010B = 1100B = C
+ *
+ *    For Figure, order 1
+ *
+ *    0      2      4      6      8      A      C      E      10
+ *    +------+------+------+------+------+------+------+------+
+ *    |      |      |      |      |      |      |      |      |
+ *    |  B0  |  B1  |  B2  |  B3  |  B4  |  B5  |  B6  |  B7  |  
+ *    |      |      |      |      |      |      |      |      |
+ *    +------+------+------+------+------+------+------+------+
+ *    | <- Pairs -> | <- Paris -> | <- Paris -> | <- Paris -> |
+ *
+ * 2) And buddy B will have and O+1 parent P which satisfies
+ *    the following equation:
+ *
+ *      P = B & ~(1 << O)
+ *
+ *    For example, if the starting buddy is #8 it order 1:
+ *
+ *      P = 0 & ~(1 << 1) = 0000B & 1101B = 0000B
+ *      P = 2 & ~(1 << 1) = 0010B & 1101B = 0000B
+ *      P = 4 & ~(1 << 1) = 0100B & 1101B = 0100B
+ *      P = 6 & ~(1 << 1) = 0110B & 1101B = 0100B
+ *      P = 8 & ~(1 << 1) = 1000B & 1101B = 1000B
+ *      P = A & ~(1 << 1) = 1010B & 1101B = 1000B
+ *      P = C & ~(1 << 1) = 1100B & 1101B = 1100B
+ *      P = E & ~(1 << 1) = 1110B & 1101B = 1100B
+ *
+ *    For Figure, order 1
+ *
+ *    0      2      4      6      8      A      C      E      10
+ *    +------+------+------+------+------+------+------+------+
+ *    |      |      |      |      |      |      |      |      |
+ *    |  B0  |  B1  |  B2  |  B3  |  B4  |  B5  |  B6  |  B7  |  
+ *    |      |      |      |      |      |      |      |      |
+ *    +------+------+------+------+------+------+------+------+
+ *    | <- Pairs -> | <- Paris -> | <- Paris -> | <- Paris -> |
+ *    P0            P1            P2            P3
+ *
+ * Assumption: *_mem_map is contiguous at least up to MAX_ORDER.
+ */
+static inline unsigned long
+__find_buddy_pfn(unsigned long page_pfn, unsigned int order)
+{
+	return page_pfn ^ (1 << order);
+}
+
+/*
+ * This function checks whether a page is free && is the buddy
+ * we can coalesce a page and its buddy if
+ * (a) the buddy is not in a hole (check before calling!) &&
+ * (b) the buddy is in the buddy system &&
+ * (c) a page and its buddy have the same order &&
+ * (d) a page and its buddy are in the same zone.
+ *
+ * For recording whether a page is in the system, we set PageBuddy.
+ * Setting, clearing, and testing PageBuddy is serialized by 
+ * zone->lock.
+ *
+ * For recording page's order, we use page_private(page);
+ */
+static inline int page_is_buddy(struct page *page, struct page *buddy,
+					unsigned int order)
+{
+	if (PageBuddy(buddy) && page_order(buddy) == order)
+		return 1;
+}
+
+/*
+ * Freeing functing for a buddy system allocator.
+ *
+ * The concept of a buddy system is to maintain direct-mapped table
+ * (containing bit values) for memory blocks of various "orders".
+ * The bottom level table contains the map for the smallest allocatable
+ * units of memory (here, pages), and each level above it describes
+ * pairs of units from the levels below, hence, "buddies".
+ * At a high level, all that happens here is marking the table entry
+ * at the bottom level available, and propagating the changes upward
+ * as necessary, plus some accounting needed to play nicely with other
+ * parts of the VM system.
+ * At each level, we keep a list of pages, which are heads of continuous
+ * free pages of length of (1 << order) and marked with PageByddy.
+ * Page's order is recorded or freeing one, we can derive the state of
+ * the other. That is, if we allocate a small block, and both were
+ * free, the remainder of the region must be split into blocks. If
+ * a block is freed, and its buddy is also free, then this triggers
+ * coalescing into a block of larger size.
+ *
+ * -- nyc
+ */
+
+static inline void __free_one_page(struct zone *zone, struct page *page, 
+			unsigned long pfn, unsigned int order)
+{
+	unsigned int max_order;
+	unsigned long buddy_pfn;
+	unsigned long combined_pfn;
+	struct page *buddy;
+
+	max_order = min_t(unsigned int, MAX_ORDER, pageblock_order + 1);
+
+continue_merging:
+	while (order < max_order - 1) {
+		buddy_pfn = __find_buddy_pfn(pfn, order);	
+		buddy = page + (buddy_pfn - pfn);
+
+		if (!pfn_valid_within(buddy_pfn))
+			goto done_merging;
+		if (!page_is_buddy(page, buddy, order))
+			goto done_merging;
+
+		/*
+		 * Our buddy is free and meger with it and move up
+		 * one order.
+		 */
+		list_del(&buddy->lru);
+		zone->free_area[order].nr_free--;
+		rmv_page_order(buddy);
+		combined_pfn = buddy_pfn & pfn;
+		page = page + (combined_pfn - pfn);
+		pfn = combined_pfn;
+		order++;
+	}
+
+done_merging:
+	set_page_order(page, order);
+
+	/*
+	 * If this is not the largest possible page, check if the buddy
+	 * of the next-highest order is free. If it is, it's possible
+	 * that pages are being free that will coalesce soon. In case,
+	 * that is happening, add the free page to the tail of the list
+	 * so it's less likely to be used soon and more likely to be meged
+	 * as a higher order page.
+	 */
+	if ((order < MAX_ORDER-2) && pfn_valid_within(buddy_pfn)) {
+		struct page *higher_page, *higher_buddy;
+
+		combined_pfn = buddy_pfn & pfn;
+		higher_page = page + (combined_pfn - pfn);
+		buddy_pfn = __find_buddy_pfn(combined_pfn, order + 1);
+		higher_buddy = higher_page + (buddy_pfn - combined_pfn);
+		if (pfn_valid_within(buddy_pfn) &&
+		    page_is_buddy(higher_page, higher_buddy, order + 1)) {
+			list_add_tail(&page->lru,
+				&zone->free_area[order].free_list[0]);
+			goto out;
+		}
+		goto out;
+	}
+
+	list_add(&page->lru, &zone->free_area[order].free_list[0]);
+out:
+	zone->free_area[order].nr_free++;
+}
+
+static void __free_pages_ok(struct page *page, unsigned int order)
+{
+	unsigned long pfn = page_to_pfn(page);
+
+	__free_one_page(page_zone(page), page, pfn, order);
+}
+
+static inline void free_the_page(struct page *page, unsigned int order)
+{
+	__free_pages_ok(page, order);
+}
+
+void __free_pages(struct page *page, unsigned int order)
+{
+	free_the_page(page, order);
+}
+
+/*
+ * The order of subdivision here is critical for the IO subsystem.
+ * Please do not alter this order without good reasons and regression
+ * testing. Specifically, as large blocks of memory are subdivided,
+ * the order in which smaller blocks are delivered depends on the
+ * order they're subdivided in this function. This is the primary
+ * factor influencing the order in which pages are delivered to the
+ * IO subsystem according to empirical testing, and this is also
+ * justified by considering the behavior of a buddy system containing
+ * a single large block of memory acted on by a series of small
+ * allocations. This behavior is a critical factor in sglist merging's
+ * success.
+ */
+static inline void expand(struct zone *zone, struct page *page,
+			int low, int high, struct free_area *area)
+{
+	unsigned long size = 1 << high;
+
+	while (high > low) {
+		area--;
+		high--;
+		size >>= 1;
+
+		list_add(&page[size].lru, &area->free_list[0]);
+		area->nr_free++;
+		set_page_order(&page[size], high);
+	}
+}
+
+/*
+ * Go through the free lists for the given migratetype and remove
+ * the smallest available page from the freelist.
+ */
+static inline
+struct page *__rmqueue_smallest(struct zone *zone, unsigned int order)
+{
+	unsigned int current_order;
+	struct free_area *area;
+	struct page *page;
+
+	/* Find a page for appropriate size in the preferred list */
+	for (current_order = order; current_order < MAX_ORDER; 
+							++current_order) {
+		area = &(zone->free_area[current_order]);
+		page = list_first_entry_or_null(&area->free_list[0],
+							struct page, lru);
+		if (!page)
+			continue;
+		list_del(&page->lru);
+		rmv_page_order(page);
+		area->nr_free--;
+		expand(zone, page, order, current_order, area);
+		return page;
+	}
+	return NULL;
+}
+
+/*
+ * Allocate a page from the given zone. Use pcplists for order-0
+ * allocations.
+ */
+static inline
+struct page *rmqueue(struct zone *zone, unsigned int order,
+						gfp_t gfp_flags)
+{
+	struct page *page;
+
+	if (likely(order == 0)) {
+		/* via PCP */;
+	}
+
+	/* We most definitely don't want callers attempting to
+	 * allocate greater than order-1 page units with __GFP_NOFAIL.
+	 */
+	page = __rmqueue_smallest(zone, order);
+	return page;
+}
+
+/*
+ * get_page_from_freelist goes through the zonelist trying to
+ * allocate a page.
+ */
+static struct page *
+get_page_from_freelist(gfp_t gfp_mask, unsigned int order)
+{
+	struct zone *zone = &BiscuitOS_zone;
+	struct page *page;
+
+	page = rmqueue(zone, order, gfp_mask);
+	return page;
+}
+
+/*
+ * This is the 'heart' of the zoned buddy allocator
+ */
+struct page *__alloc_pages(gfp_t gfp_mask, unsigned int order)
+{
+	struct page *page;
+
+	/* First allocation attempt */
+	page = get_page_from_freelist(gfp_mask, order);
+	return page;	
+}
+
+/*
+ * PHYS_OFFSET                                         
+ * | <--------------------- MEMORY_SIZE ----------------------> |
+ * +---------------+--------------------------------------------+
+ * |               |                                            |
+ * |               |                                            |
+ * |               |                                            |
+ * +---------------+--------------------------------------------+
+ * | <- mem_map -> |
+ *
+ */
+int memory_init(void)
+{
+	unsigned long start_pfn, end_pfn;
+	struct zone *zone = &BiscuitOS_zone;
+	int order, index;
+
+	/* Emulate Memory Region */
+	memory = (unsigned char *)malloc(MEMORY_SIZE);
+
+	/* Establish mem_map[] */
+	mem_map = (struct page *)(unsigned long)memory;
+
+	/* Initialize all pages */
+	nr_pages = MEMORY_SIZE / PAGE_SIZE;
+	for (index = 0; index < nr_pages; index++) {
+		struct page *page = &mem_map[index];
+
+		INIT_LIST_HEAD(&page->lru);
+		page->page_type |= PAGE_TYPE_BASE;
+	}
+
+	/* Initialize Zone */
+	for (order = 0; order < MAX_ORDER; order++) {
+		INIT_LIST_HEAD(&zone->free_area[order].free_list[0]);
+		zone->free_area[order].nr_free = 0;
+	}
+
+	/* free all page into Buddy Allocator */
+	start_pfn = PFN_UP(PHYS_OFFSET);
+	end_pfn = PFN_DOWN(PHYS_OFFSET + MEMORY_SIZE);
+
+	while (start_pfn < end_pfn) {
+		int order = min_t(unsigned int,
+					MAX_ORDER - 1UL, __ffs(start_pfn));
+
+		while (start_pfn + (1UL << order) > end_pfn)
+			order--;
+
+		/* Free page into Buddy Allocator */
+		__free_pages(pfn_to_page(start_pfn), order);
+
+		start_pfn += (1UL << order);
+	}
+
+	printf("BiscuitOS Memory: %#lx - %#lx\n", (unsigned long)PHYS_OFFSET, 
+					(unsigned long)(PHYS_OFFSET + MEMORY_SIZE));
+	printf("mem_map[] contains %#lx pages, page size %#lx\n", nr_pages,
+						(unsigned long)PAGE_SIZE);
+
+	return 0;
+}
+
+void memory_exit(void)
+{
+	free(memory);
+}
