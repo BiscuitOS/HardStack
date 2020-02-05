6bcd0dccce4d1fbd7ea00e4331e1b18fb6855a03
BuddyZhang1 <buddy.d.zhang@gmail.com>
2020-02-05 08:07:32 +0800

    Buddy Allocator: Userspace demo

---
+++ Memory-Allocator/Buddy/Userspace/core.c
@@ -0,0 +1,23 @@
+/*
+ * Buddy Allocator
+ *
+ * (C) 2020.02.02 BuddyZhang1 <buddy.zhang@aliyun.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+#include "linux/buddy.h"
+
+int main()
+{
+	memory_init();
+
+	memory_exit();
+	return 0;
+}
