d9c9028b3b51c00d4359adb49e11a218166e130c
BuddyZhang1 <buddy.d.zhang@gmail.com>
2020-03-10 14:01:17 +0800

    sync

--- fs/sysfs/API/dcache_hash/core.c
+++ fs/sysfs/API/dcache_hash/core.c
@@ -22,6 +22,7 @@
 
 /*
  * Little-endian word-at-a-time zero byte handling.
+ * more detial: https://lwn.net/Articles/501492/
  */
 
 struct word_at_a_time {
@@ -42,6 +43,8 @@ static inline unsigned long has_zero(unsigned long a, unsigned long *bits,
 #define prep_zero_mask(a, bits, c)	(bits)
 #define zero_bytemask(mask)		(mask)
 
+#define HASH_EARLY	0x00000001	/* Allocting during early boot */
+#define HASH_SMALL	0x00000002	/* sub-page allocation allocated, min shift passed via *_hash_shift */
 #define HASH_ZERO	0x00000004	/* Zero allocated hash table */
 
 static inline unsigned long create_zero_mask(unsigned long bits)
@@ -69,8 +72,11 @@ static inline unsigned long create_zero_mask(unsigned long bits)
 
 /* dentry hashtable */
 static struct hlist_bl_head *dentry_hashtable __read_mostly;
-static unsigned long dhash_entries = 32;
+static unsigned long dhash_entries;
 static unsigned int d_hash_shift __read_mostly;
+/* 512M-DDR */
+static unsigned long nr_kernel_pages = 0x1fc00;
+static unsigned long nr_all_pages = 0x1fc00;
 
 static inline unsigned int fold_hash(unsigned long x, unsigned long y)
 {
@@ -163,6 +169,15 @@ static inline struct hlist_bl_head *d_hash(unsigned int hash)
 }
 
 /*
+ * Returns the number of pages that arch has reserved but
+ * is not known to alloc_large_system_hash().
+ */
+static unsigned long arch_reserved_kernel_pages(void)
+{
+	return 0;
+}
+
+/*
  * allocate a large system hash table from vmalloc
  * - it is assumed that the hash table must contain an exact power-of-2
  *   quantity of entries
@@ -183,8 +198,47 @@ static void *alloc_large_system_hash(const char *tablename,
 	void *table = NULL;
 	gfp_t gfp_flags;
 
+	/* allow the kernel cmdline to have a say */
+	if (!numentries) {
+		/* round applicable memory size up to nearest 
+		 * megabyte */
+		numentries = nr_kernel_pages;
+		numentries -= arch_reserved_kernel_pages();
+
+		/* It isn't necessary when PAGE_SIZE >= 1MB */
+		if (PAGE_SHIFT < 20)
+			numentries = round_up(numentries, 
+					(1 << 20) / PAGE_SIZE);
+
+		/* limit to 1 bucket per 2^scale bytes of low memory */
+		if (scale > PAGE_SHIFT)
+			numentries >>= (scale - PAGE_SHIFT);
+		else
+			numentries <<= (PAGE_SHIFT - scale);
+
+		/* Make sure we've got at least a 0-order allocation */
+		if (unlikely(flags & HASH_SMALL)) {
+			/* Makes no sense without HASH_EARLY */
+			WARN_ON(!(flags & HASH_EARLY));
+			if (!(numentries >> *_hash_shift)) {
+				numentries = 1UL << *_hash_shift;
+				BUG_ON(!numentries);
+			}
+		} else if (unlikely((numentries * bucketsize) < 
+							PAGE_SIZE)) {
+			numentries = PAGE_SIZE / bucketsize;
+		}
+	}
 	numentries = roundup_pow_of_two(numentries);
+
+	/* limit allocation size to 1/16 total memory by default */
+	if (max == 0) {
+		max = ((unsigned long long)nr_all_pages << 
+						PAGE_SHIFT) >> 4;
+		do_div(max, bucketsize);
+	}
 	max = min(max, 0x80000000ULL);
+
 	if (numentries < low_limit)
 		numentries = low_limit;
 	if (numentries > max)
@@ -219,7 +273,8 @@ static void dcache_init(void)
 					&d_hash_shift,
 					NULL,
 					0,
-					PAGE_SIZE * 16);
+					0);
+	d_hash_shift = 32 - d_hash_shift;
 }
 
 /* Module initialize entry */
