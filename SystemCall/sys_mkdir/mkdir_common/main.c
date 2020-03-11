09f893ba1a1ae4836979663638b8fb76df6cf138
BuddyZhang1 <buddy.d.zhang@gmail.com>
2020-03-11 08:35:56 +0800

    sys_mkdir

--- SystemCall/sys_mkdir/mkdir_common/main.c
+++ SystemCall/sys_mkdir/mkdir_common/main.c
@@ -13,11 +13,59 @@
 #include <string.h>
 #include <sys/stat.h>
 #include <sys/types.h>
+#include <getopt.h>
 
-int main()
+static void usage(const char *program_name)
 {
+	printf("BiscuitOS: sys_mkdir helper\n");
+	printf("Usage:\n");
+	printf("      %s <-p pathname> <-m mode>\n", program_name);
+	printf("\n");
+	printf("\t-p\t--path\tThe full path for creating dentory.\n");
+	printf("\t-m\t--mode\tThe mode for creating dentory.\n\n");
+}
+
+int main(int argc, char *argv[])
+{
+	char *path = NULL;
+	char *mode = NULL;
+	int mode_value;
+	int c, hflags = 0;
+	opterr = 0;
+
+	/* options */
+	const char *short_opts = "hp:m:";
+	const struct option long_opts[] = {
+		{ "help", no_argument, NULL, 'h'},
+		{ "path", required_argument, NULL, 'p'},
+		{ "mode", required_argument, NULL, 'm'},
+		{ 0, 0, 0, 0 }
+	};
+
+	while ((c = getopt_long(argc, argv, short_opts, 
+						long_opts, NULL)) != -1) {
+		switch (c) {
+		case 'h':
+			hflags = 1;
+			break;
+		case 'p': /* Path */
+			path = optarg;
+			break;
+		case 'm': /* mode */
+			mode = optarg;
+			break;
+		default:
+			abort();
+		}
+	}
+
+	if (hflags || !path || !mode)
+		usage(argv[0]);
+
+	sscanf(mode, "%o", &mode_value);
+
 	/* mkdir on current dirent */
-	mkdir("BiscuitOS_bus", 0777);
+	mkdir(path, mode_value);
 
 	return 0;
 }
