af9bc4462f1be7b748e5f595407287ac308b0f3d
BuddyZhang1 <buddy.d.zhang@gmail.com>
2020-03-13 15:41:49 +0800

    syscall: __NR_mkdir

--- SystemCall/sys_mkdir/mkdir_common/main.c
+++ SystemCall/sys_mkdir/mkdir_common/main.c
@@ -9,11 +9,14 @@
  */
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 #include <string.h>
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <getopt.h>
+/* __NR_mkdir */
+#include <asm/unistd.h>
+/* syscall() */
+#include <unistd.h>
 
 static void usage(const char *program_name)
 {
@@ -22,14 +25,29 @@ static void usage(const char *program_name)
 	printf("      %s <-p pathname> <-m mode>\n", program_name);
 	printf("\n");
 	printf("\t-p\t--path\tThe full path for creating dentory.\n");
-	printf("\t-m\t--mode\tThe mode for creating dentory.\n\n");
+        printf("\t-m\t--mode\tThe mode for opening.\n");
+	printf("\t\t\tS_IRUSR\n");
+	printf("\t\t\tS_IWUSR\n");
+	printf("\t\t\tS_IXUSR\n");
+	printf("\t\t\tS_IRWXU\n");
+	printf("\t\t\tS_IRGRP\n");
+	printf("\t\t\tS_IWGRP\n");
+	printf("\t\t\tS_IXGRP\n");
+	printf("\t\t\tS_IRWXG\n");
+	printf("\t\t\tS_IROTH\n");
+	printf("\t\t\tS_IWOTH\n");
+	printf("\t\t\tS_IXOTH\n");
+	printf("\t\t\tS_IRWXO\n");
+	printf("\ne.g:\n");
+	printf("\t%s -p BiscuitOS_bus -m S_IRUSR,S_IWUSR,S_IRGRP\n\n",
+						program_name);
 }
 
 int main(int argc, char *argv[])
 {
 	char *path = NULL;
 	char *mode = NULL;
-	int mode_value;
+	int omode;
 	int c, hflags = 0;
 	opterr = 0;
 
@@ -62,10 +80,42 @@ int main(int argc, char *argv[])
 	if (hflags || !path || !mode)
 		usage(argv[0]);
 
-	sscanf(mode, "%o", &mode_value);
+        /* parse mode argument */
+	if (mode) {
+		if (strstr(mode, "S_IRUSR"))
+			omode |= S_IRUSR;
+		if (strstr(mode, "S_IWUSR"))
+			omode |= S_IWUSR;
+		if (strstr(mode, "S_IXUSR"))
+			omode |= S_IXUSR;
+		if (strstr(mode, "S_IRWXU"))
+			omode |= S_IRWXU;
+		if (strstr(mode, "S_IRGRP"))
+			omode |= S_IRGRP;
+		if (strstr(mode, "S_IWGRP"))
+			omode |= S_IWGRP;
+		if (strstr(mode, "S_IXGRP"))
+			omode |= S_IXGRP;
+		if (strstr(mode, "S_IRWXG"))
+			omode |= S_IRWXG;
+		if (strstr(mode, "S_IROTH"))
+			omode |= S_IROTH;
+		if (strstr(mode, "S_IWOTH"))
+			omode |= S_IWOTH;
+		if (strstr(mode, "S_IXOTH"))
+			omode |= S_IXOTH;
+		if (strstr(mode, "S_IRWXO"))
+			omode |= S_IRWXO;
+	}
 
-	/* mkdir on current dirent */
-	mkdir(path, mode_value);
+	/*
+	 * sys_mkdir()
+	 *
+	 *    SYSCALL_DEFINE2(mkdir,
+	 *                    const char __user *, pathname,
+	 *                    umode_t, mode)
+	 */
+	syscall(__NR_mkdir, path, omode);
 
 	return 0;
 }
