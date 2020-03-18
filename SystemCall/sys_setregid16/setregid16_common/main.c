8b0c5f870a084c8346caa493869f5862780337d0
BuddyZhang1 <buddy.d.zhang@gmail.com>
2020-03-18 10:39:20 +0800

    syscall: __NR_setregid16

---
+++ SystemCall/sys_setregid16/setregid16_common/main.c
@@ -0,0 +1,86 @@
+/*
+ * sys_setregid16 in C
+ *
+ * (C) 2020.03.11 BuddyZhang1 <buddy.zhang@aliyun.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <getopt.h>
+/* __NR_setregid16 */
+#include <asm/unistd.h>
+/* syscall() */
+#include <unistd.h>
+
+/* Architecture defined */
+#ifndef __NR_setregid16
+#define __NR_setregid16	71
+#endif
+
+typedef unsigned int old_gid_t;
+
+static void usage(const char *program_name)
+{
+	printf("BiscuitOS: sys_setregid16 helper\n");
+	printf("Usage:\n");
+	printf("      %s <-r rgid> <-e egid>\n", program_name);
+	printf("\n");
+	printf("\t-r\t--ruid\tThe real gid for current process.\n");
+	printf("\t-e\t--euid\tThe effect gid for current process.\n");
+	printf("\ne.g:\n");
+	printf("%s -r 269 -e 678\n\n", program_name);
+}
+
+int main(int argc, char *argv[])
+{
+	old_gid_t rgid = 0, egid = 0;
+	int c, hflags = 0;
+	opterr = 0;
+
+	/* options */
+	const char *short_opts = "hr:e:";
+	const struct option long_opts[] = {
+		{ "help", no_argument, NULL, 'h'},
+		{ "ruid", required_argument, NULL, 'r'},
+		{ "euid", required_argument, NULL, 'e'},
+		{ 0, 0, 0, 0 }
+	};
+
+	while ((c = getopt_long(argc, argv, short_opts, 
+						long_opts, NULL)) != -1) {
+		switch (c) {
+		case 'h':
+			hflags = 1;
+			break;
+		case 'r': /* Real UID */
+			sscanf(optarg, "%d", &rgid);
+			break;
+		case 'e': /* Effect UID */
+			sscanf(optarg, "%d", &egid);
+			break;
+		default:
+			abort();
+		}
+	}
+
+	if (hflags || !rgid || !egid) {
+		usage(argv[0]);
+		return 0;
+	}
+
+	/*
+	 * sys_setregid16
+	 *
+	 *    SYSCALL_DEFINE2(setregid16,
+	 *                    old_gid_t, ruid,
+	 *                    old_gid_t, euid)
+	 */
+	syscall(__NR_setregid16, rgid, egid);
+	return 0;
+}
