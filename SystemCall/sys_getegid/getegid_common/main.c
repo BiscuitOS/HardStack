e67bd87b26afe4024f4bf432ccfc00b2d5261bcb
BuddyZhang1 <buddy.d.zhang@gmail.com>
2020-03-20 16:17:25 +0800

    syscall: __NR_getegid

---
+++ SystemCall/sys_getegid/getegid_common/main.c
@@ -0,0 +1,74 @@
+/*
+ * sys_getegid in C
+ *
+ * (C) 2020.03.11 BuddyZhang1 <buddy.zhang@aliyun.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <getopt.h>
+/* __NR_getegid */
+#include <asm/unistd.h>
+/* syscall() */
+#include <unistd.h>
+
+/* Architecture defined */
+#ifndef __NR_getegid
+#define __NR_getegid	202
+#endif
+
+static void usage(const char *program_name)
+{
+	printf("BiscuitOS: sys_getegid helper\n");
+	printf("Usage:\n");
+	printf("      %s", program_name);
+	printf("\n");
+	printf("\ne.g:\n");
+	printf("%s\n\n", program_name);
+}
+
+int main(int argc, char *argv[])
+{
+	int c, hflags = 0;
+	gid_t gid = 0;
+	opterr = 0;
+
+	/* options */
+	const char *short_opts = "h";
+	const struct option long_opts[] = {
+		{ "help", no_argument, NULL, 'h'},
+		{ 0, 0, 0, 0 }
+	};
+
+	while ((c = getopt_long(argc, argv, short_opts, 
+						long_opts, NULL)) != -1) {
+		switch (c) {
+		case 'h':
+			hflags = 1;
+			break;
+		default:
+			abort();
+		}
+	}
+
+	if (hflags) {
+		usage(argv[0]);
+		return 0;
+	}
+
+	/*
+	 * sys_getegid
+	 *
+	 *    SYSCALL_DEFINE0(getegid)
+	 */
+	syscall(__NR_getegid, &gid);
+	printf("getegid: %#x\n", gid);
+
+	return 0;
+}
