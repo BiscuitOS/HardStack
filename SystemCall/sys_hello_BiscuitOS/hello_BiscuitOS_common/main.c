33432a12fa8e71fd5d4e42c662062f0d7c7bade6
BuddyZhang1 <buddy.d.zhang@gmail.com>
2020-03-22 11:45:21 +0800

    syscall: __NR_hello_BiscuitOS

---
+++ SystemCall/sys_hello_BiscuitOS/hello_BiscuitOS_common/main.c
@@ -0,0 +1,34 @@
+/*
+ * BiscuitOS Common system call
+ *
+ * (C) 2020.03.20 BuddyZhang1 <buddy.zhang@aliyun.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <getopt.h>
+/* __NR_* */
+#include <asm/unistd.h>
+/* syscall() */
+#include <unistd.h>
+
+/* Architecture defined */
+#ifndef __NR_hello_BiscuitOS
+#define __NR_hello_BiscuitOS	400
+#endif
+
+int main(void)
+{
+	/*
+	 * sys_hello_BiscuitOS
+	 */
+	syscall(__NR_hello_BiscuitOS);
+
+	return 0;
+}
