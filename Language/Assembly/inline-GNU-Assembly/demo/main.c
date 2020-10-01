c98feefddf5e9f12a03c6e333023fcbb5077da90
BuddyZhang1 <buddy.zhang@aliyun.com>
2020-10-02 07:07:50 +0800

    inline-assembly demo code

--- Language/Assembly/inline-GNU-Assembly/demo/main.c
+++ Language/Assembly/inline-GNU-Assembly/demo/main.c
@@ -1,7 +1,7 @@
 /*
- * Module Project
+ * Inline-Assembly
  *
- * (C) 2020.02.02 BuddyZhang1 <buddy.zhang@aliyun.com>
+ * (C) 2020.10.01 BuddyZhang1 <buddy.zhang@aliyun.com>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 as
@@ -11,24 +11,22 @@
 #include <linux/kernel.h>
 #include <linux/module.h>
 
-#include "linux/demo.h"
-
 /* Module initialize entry */
-static int __init Demo_init(void)
+static int __init BiscuitOS_init(void)
 {
-	bs_show();
+	asm ("nop");
 
 	return 0;
 }
 
 /* Module exit entry */
-static void __exit Demo_exit(void)
+static void __exit BiscuitOS_exit(void)
 {
 }
 
-module_init(Demo_init);
-module_exit(Demo_exit);
+module_init(BiscuitOS_init);
+module_exit(BiscuitOS_exit);
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("BiscuitOS <buddy.zhang@aliyun.com>");
-MODULE_DESCRIPTION("Module Project");
+MODULE_DESCRIPTION("Inline Assembly Project");
