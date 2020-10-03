44746ab605bdc6a628f0636aeb5e5f8360834bdc
BuddyZhang1 <buddy.zhang@aliyun.com>
2020-10-03 12:05:01 +0800

    Modules Demo Code on BiscuitOS

---
+++ Demo/modules/main.c
@@ -0,0 +1,33 @@
+/*
+ * Modules BiscuitOS Code on BiscuitOS
+ *
+ * (C) 2020.10.02 BuddyZhang1 <buddy.zhang@aliyun.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ */
+
+#include <linux/init.h>
+#include <linux/kernel.h>
+#include <linux/module.h>
+
+/* Module initialize entry */
+static int __init BiscuitOS_init(void)
+{
+	printk("Hello modules on BiscuitOS\n");
+
+	return 0;
+}
+
+/* Module exit entry */
+static void __exit BiscuitOS_exit(void)
+{
+}
+
+module_init(BiscuitOS_init);
+module_exit(BiscuitOS_exit);
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("BiscuitOS <buddy.zhang@aliyun.com>");
+MODULE_DESCRIPTION("Common Device driver on BiscuitOS");
