b573246c8fa9405cc8d6564cbd0557bc26af1d90
BuddyZhang1 <buddy.zhang@aliyun.com>
2020-10-03 13:06:39 +0800

    Application Project demo on BiscuitOS

---
+++ Demo/Application-project/main.c
@@ -0,0 +1,21 @@
+/*
+ * Application Project
+ *
+ * (C) 2020.02.02 BuddyZhang1 <buddy.zhang@aliyun.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+#include "linux/demo.h"
+
+int main()
+{
+	BiscuitOS_show();
+	return 0;
+}
