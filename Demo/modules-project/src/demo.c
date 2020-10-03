378861ee1ee2f48c28179b0a26f9914bbf97042b
BuddyZhang1 <buddy.zhang@aliyun.com>
2020-10-03 12:16:50 +0800

    Module Project on BiscuitOS

---
+++ Demo/modules-project/src/demo.c
@@ -0,0 +1,21 @@
+/*
+ * Module Project
+ *
+ * (C) 2020.02.02 BuddyZhang1 <buddy.zhang@aliyun.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ */
+#include <linux/init.h>
+#include <linux/kernel.h>
+#include <linux/fs.h>
+
+#include "linux/demo.h"
+
+int BiscuitOS_show(void)
+{
+	printk("Hello BiscuitOS\n");
+
+	return 0;
+}
