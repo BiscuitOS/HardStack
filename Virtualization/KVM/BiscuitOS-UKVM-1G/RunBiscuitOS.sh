5f280eb4abd2a03fe4713cd4b723c35488861ae6
BuddyZhang1 <buddy.zhang@aliyun.com>
2020-11-22 14:14:31 +0800

    KVM support 1Gig

---
+++ Virtualization/KVM/BiscuitOS-UKVM-1G/RunBiscuitOS.sh
@@ -0,0 +1,9 @@
+#!/bin/ash
+# 
+# Establish HugePage for KVM
+
+mkdir -p /mnt/HugePagefs
+mount none /mnt/HugePagefs -t hugetlbfs -o pagesize=1G
+
+echo 1 > /proc/sys/vm/nr_hugepages
+BiscuitOS-UKVM-2M-default
