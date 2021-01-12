40aa422a200a4ad0f9c6016d036e6dedff458592
BuddyZhang1 <buddy.zhang@aliyun.com>
2021-01-13 07:52:30 +0800

    nodemask

---
+++ Algorithem/nodemask/node_test_and_set/main.c
@@ -0,0 +1,47 @@
+/*
+ * Nodemask: node_test_and_set
+ *
+ * (C) 2021.01.10 BuddyZhang1 <buddy.zhang@aliyun.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ */
+
+#include <linux/init.h>
+#include <linux/kernel.h>
+#include <linux/module.h>
+
+/* Module initialize entry */
+static int __init BiscuitOS_init(void)
+{
+	nodemask_t node;
+
+	/* Clear all bits */
+	nodes_clear(node);
+
+	if (!node_test_and_set(2, node))
+		printk("NODE 2 is clear.\n");
+
+	if (node_isset(2, node))
+		printk("NODE 2 is set.\n");
+
+	if (node_set_and_test(2, node))
+		printk("NODE 2 has set.\n");
+
+	printk("Hello modules on BiscuitOS\n");
+
+	return 0;
+}
+
+/* Module exit entry */
+static void __exit BiscuitOS_exit(void)
+{
+}
+
+module_init(BiscuitOS_init);
+module_exit(BiscuitOS_exit);
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("BiscuitOS <buddy.zhang@aliyun.com>");
+MODULE_DESCRIPTION("Common Device driver on BiscuitOS");
