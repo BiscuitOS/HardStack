785fc539fa913635957021c585e77276c2244944
BuddyZhang1 <buddy.zhang@aliyun.com>
2021-01-15 07:20:03 +0800

    s

---
+++ Algorithem/nodemask/nodes_intersects/main.c
@@ -0,0 +1,48 @@
+/*
+ * Nodemask: nodes_intersects
+ *
+ * (C) 2021.01.10 BuddyZhang1 <buddy.zhang@aliyun.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ */
+
+#include <linux/init.h>
+#include <linux/kernel.h>
+#include <linux/module.h>
+
+/* Module initialize entry */
+static int __init BiscuitOS_init(void)
+{
+        nodemask_t node_a;
+        nodemask_t node_b;
+
+        /* Clear all bits */
+        nodes_clear(node_a);
+        nodes_clear(node_b);
+
+        /* Set special bits */
+        node_set(2, node_a);
+        node_set(4, node_a);
+        node_set(2, node_b);
+
+        if (nodes_intersects(node_a, node_b))
+                printk("Intersect between a and b.\n");
+
+        printk("Hello modules on BiscuitOS\n");
+
+        return 0;
+}
+
+/* Module exit entry */
+static void __exit BiscuitOS_exit(void)
+{
+}
+
+module_init(BiscuitOS_init);
+module_exit(BiscuitOS_exit);
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("BiscuitOS <buddy.zhang@aliyun.com>");
+MODULE_DESCRIPTION("Common Device driver on BiscuitOS");
