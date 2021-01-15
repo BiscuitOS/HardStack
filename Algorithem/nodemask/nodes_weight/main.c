21750383398a8d43ebbfacf985fe374ed8b44061
BuddyZhang1 <buddy.zhang@aliyun.com>
2021-01-15 08:12:48 +0800

    s

---
+++ Algorithem/nodemask/nodes_weight/main.c
@@ -0,0 +1,46 @@
+/*
+ * Nodemask: nodes_weight
+ *
+ * (C) 2021.01.10 BuddyZhang1 <buddy.zhang@aliyun.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ */
+
+#include <linux/init.h>
+#include <linux/kernel.h>
+#include <linux/module.h>
+
+/* Module initialize entry */
+static int __init BiscuitOS_init(void)
+{
+        nodemask_t node_a;
+        int weight;
+
+        /* Clear all bits */
+        nodes_clear(node_a);
+
+        /* Set special bits */
+        node_set(2, node_a);
+        node_set(4, node_a);
+
+        weight = nodes_weight(node_a);
+        printk("NODE A weight %d\n", weight);
+
+        printk("Hello modules on BiscuitOS\n");
+
+        return 0;
+}
+
+/* Module exit entry */
+static void __exit BiscuitOS_exit(void)
+{
+}
+
+module_init(BiscuitOS_init);
+module_exit(BiscuitOS_exit);
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("BiscuitOS <buddy.zhang@aliyun.com>");
+MODULE_DESCRIPTION("Common Device driver on BiscuitOS");
