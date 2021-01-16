0a9c2991f63621c7526ac9acc9fc85ee47d8d29c
BuddyZhang1 <buddy.zhang@aliyun.com>
2021-01-16 13:05:16 +0800

    s

---
+++ Algorithem/nodemask/for_each_node_mask/main.c
@@ -0,0 +1,47 @@
+/*
+ * Nodemask: for_each_node_mask
+ *
+ * (C) 2021.01.10 BuddyZhang1 <buddy.zhang@aliyun.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ */
+
+#include <linux/init.h>
+#include <linux/kernel.h>
+#include <linux/module.h>
+
+/* Module initialize entry */
+static int __init BiscuitOS_init(void)
+{
+        nodemask_t node_a;
+        int idx;
+
+        /* Clear all */
+        nodes_clear(node_a);
+
+        /* set special node */
+        node_set(12, node_a);
+        node_set(16, node_a);
+
+        /* iterator */
+        for_each_node_mask(idx, node_a)
+                printk("Online-node: %d\n", idx);
+
+        printk("Hello modules on BiscuitOS\n");
+
+        return 0;
+}
+
+/* Module exit entry */
+static void __exit BiscuitOS_exit(void)
+{
+}
+
+module_init(BiscuitOS_init);
+module_exit(BiscuitOS_exit);
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("BiscuitOS <buddy.zhang@aliyun.com>");
+MODULE_DESCRIPTION("Common Device driver on BiscuitOS");
