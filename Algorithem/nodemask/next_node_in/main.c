4037cf1d3858e395908ac40beda1b38df9ba9e82
BuddyZhang1 <buddy.zhang@aliyun.com>
2021-01-16 11:30:03 +0800

    sync

---
+++ Algorithem/nodemask/next_node_in/main.c
@@ -0,0 +1,44 @@
+/*
+ * Nodemask:next_node_in
+ *
+ * (C) 2021.01.10 BuddyZhang1 <buddy.zhang@aliyun.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ */
+
+#include <linux/init.h>
+#include <linux/kernel.h>
+#include <linux/module.h>
+
+/* Module initialize entry */
+static int __init BiscuitOS_init(void)
+{
+        nodemask_t node_a;
+
+        /* Clear all bits */
+        nodes_clear(node_a);
+
+        /* Set special bits */
+        node_set(12, node_a);
+
+        /* next lowest set bit */
+        printk("The next bit for 13th: %d\n", next_node_in(13, node_a));
+
+        printk("Hello modules on BiscuitOS\n");
+
+        return 0;
+}
+
+/* Module exit entry */
+static void __exit BiscuitOS_exit(void)
+{
+}
+
+module_init(BiscuitOS_init);
+module_exit(BiscuitOS_exit);
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("BiscuitOS <buddy.zhang@aliyun.com>");
+MODULE_DESCRIPTION("Common Device driver on BiscuitOS");
