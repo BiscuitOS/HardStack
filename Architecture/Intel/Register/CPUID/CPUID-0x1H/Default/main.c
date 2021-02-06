25e476840e96516a07dfab5a9a60969bb409dc12
BuddyZhang1 <buddy.zhang@aliyun.com>
2021-02-06 14:32:41 +0800

    s

---
+++ Architecture/Intel/Register/CPUID/CPUID-0x1H/Default/main.c
@@ -0,0 +1,51 @@
+/*
+ * CPUID.01H
+ *
+ * (C) 2020.10.02 BuddyZhang1 <buddy.zhang@aliyun.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ */
+
+#include <linux/init.h>
+#include <linux/kernel.h>
+#include <linux/module.h>
+/* CPUID */
+#include <asm/processor.h>
+#include <asm/cpufeatures.h>
+
+/* Module initialize entry */
+static int __init BiscuitOS_init(void)
+{
+	unsigned int eax, ebx, ecx, edx;
+
+	eax = ebx = ecx = edx = 0;
+
+	/* CPUID 01H */
+	cpuid(0x01, &eax, &ebx, &ecx, &edx);
+
+	printk("CPUID 0x01H\n");
+	printk("\tEAX: %#x\n\tEBX: %#x\n\tECX: %#x\n\tEDX: %#x\n",
+						eax, ebx, ecx, edx);
+
+	/* Page Size Extensions */
+	if (edx & X86_FEATURE_PSE)
+		printk("CPU Feature Support: X86_FEATURE_PSE\n");
+
+	printk("Hello modules on BiscuitOS\n");
+
+	return 0;
+}
+
+/* Module exit entry */
+static void __exit BiscuitOS_exit(void)
+{
+}
+
+module_init(BiscuitOS_init);
+module_exit(BiscuitOS_exit);
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("BiscuitOS <buddy.zhang@aliyun.com>");
+MODULE_DESCRIPTION("CPUID on BiscuitOS");
