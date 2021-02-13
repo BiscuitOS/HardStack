ec73dcadca82e0d152a5d60b8b9baa241810a942
BuddyZhang1 <buddy.zhang@aliyun.com>
2021-02-13 13:50:32 +0800

    sync

--- Memory-Allocator/Paging/paging_mode_32bit/Paging-32bit-4MB-Page/kernel/RunBiscuitOS.sh
+++ Memory-Allocator/Paging/paging_mode_32bit/Paging-32bit-4MB-Page/kernel/RunBiscuitOS.sh
@@ -1,4 +1,4 @@
 #!/bin/ash
 
-insmod /lib/modules/$(uname -r)/extra/X86-Paging-32bit-4K-Page-kernel-default.ko
-X86-Paging-32bit-4K-Page-userspace-default
+insmod /lib/modules/$(uname -r)/extra/X86-Paging-32bit-4M-Page-kernel-default.ko
+X86-Paging-32bit-4M-Page-userspace-default
