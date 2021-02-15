ce1dd2532e547a1e8931ec6ee1802c122462a1fb
BuddyZhang1 <buddy.zhang@aliyun.com>
2021-02-15 18:37:24 +0800

    s

--- Memory-Allocator/Paging/paging_mode_32bit/Paging-32bit-FIXMAP/main.c
+++ Memory-Allocator/Paging/paging_mode_32bit/Paging-32bit-FIXMAP/main.c
@@ -19,6 +19,34 @@
 /* counter */
 static int BiscuitOS_fixmaps_set;
 
+static void BiscuitOS_set_pte_vaddr(unsigned long vaddr, pte_t pteval)
+{
+	pgd_t *pgd;
+	pmd_t *pde;
+	pte_t *pte;
+
+	pgd = swapper_pg_dir + pgd_index(vaddr);
+	if (pgd_none(*pgd)) {
+		BUG();
+		return;
+	}
+
+	/* Only contains PDE and PTE on 32-bit Paging */
+	pde = (pmd_t *)pgd;
+
+	pte = pte_offset_kernel(pde, vaddr);
+	if (!pte_none(pteval))
+		set_pte_at(&init_mm, vaddr, pte, pteval);
+	else
+		pte_clear(&init_mm, vaddr, pte);
+
+	/*
+ 	 * It's enough to flush this one mapping.
+	 * (PGE mappins get flushed at well)
+	 */
+	__flush_tlb_one_kernel(vaddr);
+}
+
 static void __BiscuitOS_native_set_fixmap(enum fixed_addresses idx, pte_t pte)
 {
 	unsigned long address = __fix_to_virt(idx);
@@ -27,7 +55,7 @@ static void __BiscuitOS_native_set_fixmap(enum fixed_addresses idx, pte_t pte)
 		BUG();
 		return;
 	}
-	set_pte_vaddr(address, pte);
+	BiscuitOS_set_pte_vaddr(address, pte);
 	BiscuitOS_fixmaps_set++;
 }
 
@@ -66,6 +94,8 @@ static int __init BiscuitOS_init(void)
 
 	printk("\n\n\n\n*************BiscuitOS*************\n");
 	printk("=> %#lx: %ld\n", (unsigned long)addr, *addr);
+	printk("FIXMAP Range: %#lx - %#lx\n", 
+		fix_to_virt(FIX_HOLE), fix_to_virt(__end_of_fixed_addresses-1));
 	printk("**********************************\n\n\n\n");
 
 	/* FIXED Unmap */
