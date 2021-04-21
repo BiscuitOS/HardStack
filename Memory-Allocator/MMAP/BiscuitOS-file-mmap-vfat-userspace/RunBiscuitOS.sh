cb43b87b7148ccd715589d429cfa04e952c5b080
BuddyZhang1 <buddy.zhang@aliyun.com>
2021-04-21 21:47:57 +0800

    s

--- Memory-Allocator/MMAP/BiscuitOS-file-mmap-vfat-userspace/RunBiscuitOS.sh
+++ Memory-Allocator/MMAP/BiscuitOS-file-mmap-vfat-userspace/RunBiscuitOS.sh
@@ -25,6 +25,6 @@ BiscuitOS-file-mmap-vfat-userspace-default
 # hexdump
 if [ -f /usr/bin/hexdump ]; then
   echo ""
-  echo "Dump Context for /BiscuitOS-vfat/BiscuitOS             [YES]"
+  echo "Dump Context for /BiscuitOS-vfat/BiscuitOS            [YES]"
   hexdump /BiscuitOS-vfat/BiscuitOS
 fi
