ac0c3e5f6a01331f48c4e38e345e4c8c1206b15d
BuddyZhang1 <buddy.zhang@aliyun.com>
2021-04-21 15:45:05 +0800

    s

--- Memory-Allocator/MMAP/BiscuitOS-file-private-mmap-userspace/RunBiscuitOS.sh
+++ Memory-Allocator/MMAP/BiscuitOS-file-private-mmap-userspace/RunBiscuitOS.sh
@@ -1,7 +1,7 @@
 #!/bin/ash
 
 echo "BiscuitOS" > /BiscuitOS
-BiscuitOS-file-share-mmap-userspace-default
+BiscuitOS-file-private-mmap-userspace-default
 # dump
 echo ""
 echo "Dump Context for /BiscuitOS"
