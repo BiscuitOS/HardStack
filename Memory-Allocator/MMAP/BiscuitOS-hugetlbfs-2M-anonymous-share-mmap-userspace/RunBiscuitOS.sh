efa0d255fd3a38a2b17a5c0f7c8f891132b6c4e5
BuddyZhang1 <buddy.zhang@aliyun.com>
2021-04-21 16:23:37 +0800

    s

---
+++ Memory-Allocator/MMAP/BiscuitOS-hugetlbfs-2M-anonymous-share-mmap-userspace/RunBiscuitOS.sh
@@ -0,0 +1,6 @@
+#!/bin/ash
+# mount hugetlbfs
+mkdir -p /mnt/BiscuitOS-hugetlbfs/
+mount -t hugetlbfs none /mnt/BiscuitOS-hugetlbfs/ -o pagesize=2048K
+echo 20 > /proc/sys/vm/nr_hugepages
+BiscuitOS-hugetlbfs-2M-anonymous-share-mmap-userspace-default
