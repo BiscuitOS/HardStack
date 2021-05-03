147f149f1f861bd2cd44ee11679be047b52e66fd
BuddyZhang1 <buddy.zhang@aliyun.com>
2021-05-03 16:01:10 +0800

    s

---
+++ Memory-Allocator/MMAP/BiscuitOS-sys-mmap/BiscuitOS-sys-mmap-example-00/RunBiscuitOS.sh
@@ -0,0 +1,19 @@
+#!/bin/ash
+
+# Mount tmpfs
+mkdir -p /BiscuitOS-tmpfs/
+mount -t tmpfs -o noexec nodev /BiscuitOS-tmpfs/
+cd /BiscuitOS-tmpfs/
+echo "Mount information                                     [YES]"
+mount | grep BiscuitOS
+echo ""
+
+echo "Invoke Process                                        [YES]"
+BiscuitOS-file-mmap-tmpfs-userspace-default
+
+# hexdump
+if [ -f /usr/bin/hexdump ]; then
+  echo ""
+  echo "Dump Context for /BiscuitOS-tmpfs/BiscuitOS           [YES]"
+  hexdump /BiscuitOS-tmpfs/BiscuitOS
+fi
