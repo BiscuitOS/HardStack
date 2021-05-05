5d5a6ede000e57a468f19fd3fe2d583d90171671
BuddyZhang1 <buddy.zhang@aliyun.com>
2021-05-05 08:55:25 +0800

    s

---
+++ Memory-Allocator/MMAP/BiscuitOS-sys-mmap/BiscuitOS-sys-mmap-example-04/main.c
@@ -0,0 +1,35 @@
+/*
+ * NULL on Kernel Space
+ *
+ * (C) 2021.05.01 BuddyZhang1 <buddy.zhang@aliyun.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ */
+
+#include <linux/init.h>
+#include <linux/kernel.h>
+#include <linux/module.h>
+
+/* Module initialize entry */
+static int __init BiscuitOS_init(void)
+{
+	int *p = NULL;
+
+	*p = 88520;
+
+	return 0;
+}
+
+/* Module exit entry */
+static void __exit BiscuitOS_exit(void)
+{
+}
+
+module_init(BiscuitOS_init);
+module_exit(BiscuitOS_exit);
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("BiscuitOS <buddy.zhang@aliyun.com>");
+MODULE_DESCRIPTION("Memory Mapping on BiscuitOS");
