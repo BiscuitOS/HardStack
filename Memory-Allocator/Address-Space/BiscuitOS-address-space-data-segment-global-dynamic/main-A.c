9006166afa43d36e85e16c9c8233a7c64363c7a5
BuddyZhang1 <buddy.zhang@aliyun.com>
2021-05-29 11:19:04 +0800

    s

--- Memory-Allocator/Address-Space/BiscuitOS-address-space-data-segment-global-dynamic/main-A.c
+++ Memory-Allocator/Address-Space/BiscuitOS-address-space-data-segment-global-dynamic/main-A.c
@@ -20,12 +20,47 @@ unsigned long BiscuitOS_current_global_init_variable = 52088;
 
 int main()
 {
+	extern char __executable_start[];
+	extern char edata[];
+	extern char etext[];
+	extern char end[];
+
+	printf("***************************************************************\n");
+	printf("+---+-------+-------+------+--+------+-+------+--------------+\n");
+	printf("|   |       |       |      |  |      | |      |              |\n");
+	printf("|   | .text | .data | .bss |  | Heap | | Mmap |        Stack |\n");
+	printf("|   |       |       |      |  |      | |      |              |\n");
+	printf("+---+-------+-------+------+--+------+-+------+--------------+\n");
+	printf("0                                                            TASK_SIZE\n");
+	printf("Executable start: %#016lx\n", (unsigned long)__executable_start);
+	printf("Code Range:       %#016lx -- %#016lx\n", 
+						(unsigned long)__executable_start,
+						(unsigned long)etext);
+	printf("Data Range:       %#016lx -- %#016lx\n", (unsigned long)etext,
+						(unsigned long)edata);
+	printf("BSS  Range:       %#016lx -- %#016lx\n", (unsigned long)edata,
+						(unsigned long)end);
+	printf("***************************************************************\n");
+
 	/* Direct ref */
 	BiscuitOS_other_global_uninit_variable   = 88520;
 	BiscuitOS_other_global_init_variable     = 88521;
 
 	BiscuitOS_current_global_uninit_variable = 88522;
 	BiscuitOS_current_global_init_variable   = 88523;
+
+	printf("A: BiscuitOS_other_global_uninit_variable:   %#lx - %ld\n",
+				&BiscuitOS_other_global_uninit_variable,
+				BiscuitOS_other_global_uninit_variable);
+	printf("A: BiscuitOS_other_global_init_variable:     %#lx - %ld\n",
+				&BiscuitOS_other_global_init_variable,
+				BiscuitOS_other_global_init_variable);
+	printf("A: BiscuitOS_current_global_uninit_variable: %#lx - %ld\n",
+				&BiscuitOS_current_global_uninit_variable,
+				BiscuitOS_current_global_uninit_variable);
+	printf("A: BiscuitOS_current_global_init_variable:   %#lx - %ld\n",
+				&BiscuitOS_current_global_init_variable,
+				BiscuitOS_current_global_init_variable);
 
 	/* only debug */
 	printf("Main A PID %ld\n", (long)getpid());
--- Memory-Allocator/Address-Space/BiscuitOS-address-space-data-segment-global-dynamic/main-B.c
+++ Memory-Allocator/Address-Space/BiscuitOS-address-space-data-segment-global-dynamic/main-B.c
@@ -18,9 +18,38 @@ extern unsigned long BiscuitOS_other_global_init_variable;
 
 int main()
 {
+	extern char __executable_start[];
+	extern char edata[];
+	extern char etext[];
+	extern char end[];
+
+	printf("***************************************************************\n");
+	printf("+---+-------+-------+------+--+------+-+------+--------------+\n");
+	printf("|   |       |       |      |  |      | |      |              |\n");
+	printf("|   | .text | .data | .bss |  | Heap | | Mmap |        Stack |\n");
+	printf("|   |       |       |      |  |      | |      |              |\n");
+	printf("+---+-------+-------+------+--+------+-+------+--------------+\n");
+	printf("0                                                            TASK_SIZE\n");
+	printf("Executable start: %#016lx\n", (unsigned long)__executable_start);
+	printf("Code Range:       %#016lx -- %#016lx\n", 
+						(unsigned long)__executable_start,
+						(unsigned long)etext);
+	printf("Data Range:       %#016lx -- %#016lx\n", (unsigned long)etext,
+						(unsigned long)edata);
+	printf("BSS  Range:       %#016lx -- %#016lx\n", (unsigned long)edata,
+						(unsigned long)end);
+	printf("***************************************************************\n");
+
 	/* Direct ref */
 	BiscuitOS_other_global_uninit_variable   = 88526;
 	BiscuitOS_other_global_init_variable     = 88527;
+
+	printf("B: BiscuitOS_other_global_uninit_variable: %#lx - %ld\n",
+				&BiscuitOS_other_global_uninit_variable,
+				BiscuitOS_other_global_uninit_variable);
+	printf("B: BiscuitOS_other_global_init_variable:   %#lx - %ld\n",
+				&BiscuitOS_other_global_init_variable,
+				BiscuitOS_other_global_init_variable);
 
 	/* only debug */
 	printf("Main B PID %ld\n", (long)getpid());
