dcf817365f76adcaca597c8a0024e4b2b95549cc
BuddyZhang1 <buddy.d.zhang@gmail.com>
2021-09-10 20:11:06 +0800

    Shell

--- Language/Shell/BiscuitOS-Shell/main.sh
+++ Language/Shell/BiscuitOS-Shell/main.sh
@@ -1,13 +1,11 @@
-#!/usr/bin/python
+#!/bin/ash
 #
-# Python Language on BiscuitOS
+# Shell Language on BiscuitOS
 #
 # (C) 2021.09.09 BuddyZhang1 <buddy.zhang@aliyun.com>
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License version 2 as
 # published by the Free Software Foundation.
-import sys
 
-if __name__ == '__main__':
-	print "Hello Python on BiscuitOS"
+echo "Hello Shell on BiscuitOS"
