349bc12f2e1cc468f48a0cda42f0bec7481d5787
BuddyZhang1 <buddy.d.zhang@gmail.com>
2021-09-10 19:51:09 +0800

    python

---
+++ Language/Python/BiscuitOS-Python/main.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+#
+# C++ Language on BiscuitOS
+#
+# (C) 2021.09.09 BuddyZhang1 <buddy.zhang@aliyun.com>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 2 as
+# published by the Free Software Foundation.
+import sys
+
+if __name__ == '__main__':
+	print "Hello Python on BiscuitOS"
