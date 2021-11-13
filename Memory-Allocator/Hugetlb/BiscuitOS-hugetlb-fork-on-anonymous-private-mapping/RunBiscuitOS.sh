c4756321611b11a4379d5152d145c7643c80088f
BuddyZhang1 <buddy.d.zhang@gmail.com>
2021-11-14 06:32:27 +0800

    fixup

--- Memory-Allocator/Hugetlb/BiscuitOS-hugetlb-fork-on-anonymous-private-mapping/RunBiscuitOS.sh
+++ Memory-Allocator/Hugetlb/BiscuitOS-hugetlb-fork-on-anonymous-private-mapping/RunBiscuitOS.sh
@@ -1,3 +1,4 @@
 #!/bin/ash
-echo 1 > /sys/kernel/mm/hugepages/hugepages-2048kB/nr_hugepages
+# Child and Parent need 2 hugepages
+echo 2 > /sys/kernel/mm/hugepages/hugepages-2048kB/nr_hugepages
 BiscuitOS-hugetlb-fork-on-anonymous-private-mapping-default
