6318a1ec1a6e9eb4f3593a64ef0c08915df314d6
BuddyZhang1 <buddy.d.zhang@gmail.com>
2021-11-13 14:07:24 +0800

    fix

--- Memory-Allocator/Hugetlb/BiscuitOS-hugetlb-file-private-mapping-2MiB/RunBiscuitOS.sh
+++ Memory-Allocator/Hugetlb/BiscuitOS-hugetlb-file-private-mapping-2MiB/RunBiscuitOS.sh
@@ -3,4 +3,4 @@
 mkdir -p /mnt/BiscuitOS-hugetlbfs/
 echo 1 > /sys/kernel/mm/hugepages/hugepages-2048kB/nr_hugepages
 mount -t hugetlbfs none /mnt/BiscuitOS-hugetlbfs/ -o pagesize=2048K
-BiscuitOS-hugetlb-file-private-mapping-2MiB
+BiscuitOS-hugetlb-file-private-mapping-2MiB-default
