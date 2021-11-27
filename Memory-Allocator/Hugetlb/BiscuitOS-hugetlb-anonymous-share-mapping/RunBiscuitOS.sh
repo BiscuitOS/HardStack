9a637865494846078aafc9f67b9226165f00ca77
BuddyZhang1 <buddy.d.zhang@gmail.com>
2021-11-27 19:58:24 +0800

    hugeltb: anonymous shared for default size hugepage

---
+++ Memory-Allocator/Hugetlb/BiscuitOS-hugetlb-anonymous-share-mapping/RunBiscuitOS.sh
@@ -0,0 +1,8 @@
+#!/bin/ash
+# 
+# Allocate 10 Hugepage to default hugepage pool
+echo 10 > /sys/kernel/mm/hugepages/hugepages-2048kB/nr_hugepages
+# Running program
+BiscuitOS-hugetlb-anonymous-share-mapping-2MiB-default
+# Information for default hugepage pool
+cat /proc/meminfo | grep Huge
