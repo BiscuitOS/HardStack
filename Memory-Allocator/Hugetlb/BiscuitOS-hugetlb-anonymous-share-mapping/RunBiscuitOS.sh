2587136590b597be1f11cdb2512bcda9ca08d2b0
BuddyZhang1 <buddy.d.zhang@gmail.com>
2021-11-27 20:01:15 +0800

    update and fix bug

--- Memory-Allocator/Hugetlb/BiscuitOS-hugetlb-anonymous-share-mapping/RunBiscuitOS.sh
+++ Memory-Allocator/Hugetlb/BiscuitOS-hugetlb-anonymous-share-mapping/RunBiscuitOS.sh
@@ -3,6 +3,6 @@
 # Allocate 10 Hugepage to default hugepage pool
 echo 10 > /sys/kernel/mm/hugepages/hugepages-2048kB/nr_hugepages
 # Running program
-BiscuitOS-hugetlb-anonymous-share-mapping-2MiB-default
+BiscuitOS-hugetlb-anonymous-share-mapping-default
 # Information for default hugepage pool
 cat /proc/meminfo | grep Huge
