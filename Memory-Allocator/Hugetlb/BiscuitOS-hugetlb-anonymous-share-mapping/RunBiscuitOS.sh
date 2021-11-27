f19641649aa1c5447768333d7db6cd540afe374d
BuddyZhang1 <buddy.d.zhang@gmail.com>
2021-11-27 20:03:28 +0800

    [PATCH] hugetlb: anonymous shoared for default size hugepage

--- Memory-Allocator/Hugetlb/BiscuitOS-hugetlb-anonymous-share-mapping/RunBiscuitOS.sh
+++ Memory-Allocator/Hugetlb/BiscuitOS-hugetlb-anonymous-share-mapping/RunBiscuitOS.sh
@@ -1,7 +1,7 @@
 #!/bin/ash
 # 
 # Allocate 10 Hugepage to default hugepage pool
-echo 10 > /sys/kernel/mm/hugepages/hugepages-2048kB/nr_hugepages
+echo 10 > /proc/sys/vm/nr_hugepages
 # Running program
 BiscuitOS-hugetlb-anonymous-share-mapping-default
 # Information for default hugepage pool
