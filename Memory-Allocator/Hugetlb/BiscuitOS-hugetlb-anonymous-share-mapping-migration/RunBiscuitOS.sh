e474fa5ac6ceef194ebb460468294ebbf5230394
BuddyZhang1 <buddy.d.zhang@gmail.com>
2021-11-29 08:37:47 +0800

    fix bug

--- Memory-Allocator/Hugetlb/BiscuitOS-hugetlb-anonymous-share-mapping-migration/RunBiscuitOS.sh
+++ Memory-Allocator/Hugetlb/BiscuitOS-hugetlb-anonymous-share-mapping-migration/RunBiscuitOS.sh
@@ -5,6 +5,7 @@ echo 10 > /proc/sys/vm/nr_hugepages
 # Running program
 BiscuitOS-hugetlb-anonymous-share-mapping-migration-default &
 # Information for default hugepage pool
+sleep 2
 cat /sys/devices/system/node/node0/meminfo | grep Huge
 cat /sys/devices/system/node/node1/meminfo | grep Huge
 sleep 25
