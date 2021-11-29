9da97d725725b24d517232eaaefea18e62af259f
BuddyZhang1 <buddy.d.zhang@gmail.com>
2021-11-29 08:31:19 +0800

    hugetlb: Shared anonymous hugetlb for migration

---
+++ Memory-Allocator/Hugetlb/BiscuitOS-hugetlb-anonymous-share-mapping-migration/RunBiscuitOS.sh
@@ -0,0 +1,12 @@
+#!/bin/ash
+# 
+# Allocate 10 Hugepage to default hugepage pool
+echo 10 > /proc/sys/vm/nr_hugepages
+# Running program
+BiscuitOS-hugetlb-anonymous-share-mapping-migration-default &
+# Information for default hugepage pool
+cat /sys/device/system/node/node0/meminfo | grep Huge
+cat /sys/device/system/node/node1/meminfo | grep Huge
+sleep 25
+cat /sys/device/system/node/node0/meminfo | grep Huge
+cat /sys/device/system/node/node1/meminfo | grep Huge
