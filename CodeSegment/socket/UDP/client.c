a76361ccd7253fa9dadc8b4ddcf65e50fd121577
BuddyZhang1 <buddy.d.zhang@gmail.com>
2021-12-03 07:37:32 +0800

    Socket: UDP

---
+++ CodeSegment/socket/UDP/client.c
@@ -0,0 +1,57 @@
+/*
+ * Socket Client (UDP).
+ *
+ * (C) 2021.12.03 <buddy.zhang@aliyun.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ */
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>			/* See NOTES */
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <string.h>
+#include <netinet/in.h>
+#include <stdlib.h>
+#include <errno.h>
+
+/* Define IP */
+#define SOCKET_IP	"127.0.0.1"
+
+/* Define Port */
+#define SOCKET_PORT	8810
+
+/* Buffer size */
+#define MES_BUFSZ	10240
+
+int main(void)
+{
+	struct sockaddr_in server_address;
+	char sendbuf[MES_BUFSZ] = {0};
+	char recvbuf[MES_BUFSZ] = {0};
+	int socket_fd;
+	int server_len;
+	int sock;	
+
+	if ((socket_fd = socket(PF_INET, SOCK_DGRAM, 0)) < 0) {
+		perror("CREATE SOCKET");
+		exit(-1);
+	}
+
+	memset(&server_address, 0, sizeof(struct sockaddr_in));
+	server_address.sin_family = AF_INET;
+	/* Configuration IP and Port */
+	server_address.sin_addr.s_addr = inet_addr(SOCKET_IP);
+	server_address.sin_port = htons(SOCKET_PORT);
+
+	/* Send */
+	sprintf(sendbuf, "Hello %s", "BiscuitOS");
+	printf(" |-> Client-Send: %s\n", sendbuf);
+	sendto(socket_fd, sendbuf, strlen(sendbuf), 0,
+			(struct sockaddr *)&server_address, sizeof(server_address));
+
+	close(socket_fd);
+	return 0;
+}
---
+++ CodeSegment/socket/UDP/server.c
@@ -0,0 +1,67 @@
+/*
+ * Socket Server (UDP).
+ *
+ * (C) 2021.12.03 <buddy.zhang@aliyun.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ */
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>			/* See NOTES */
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <string.h>
+#include <netinet/in.h>
+#include <stdlib.h>
+#include <errno.h>
+
+/* Define PORT */
+#define SOCKET_PORT	 8810
+
+int main(void)
+{
+	struct sockaddr_in server_address, client_address;
+	int server_sockfd, client_sockfd;	
+	int server_len, client_len;
+	char recvbuf[1024];
+	int ret;
+
+	printf("** UDP Socket **\n");
+	/* UDP socket */
+	if ((server_sockfd = socket(PF_INET, SOCK_DGRAM, 0)) < 0)
+		printf("SOCKET ERROR.\n");
+	
+	server_len = sizeof(struct sockaddr_in);
+	memset(&server_address, 0, server_len);
+	server_address.sin_family = AF_INET;
+	/* Configureation IP and PORT */
+	server_address.sin_port = htons(SOCKET_PORT);
+	server_address.sin_addr.s_addr = htonl(INADDR_ANY);
+	server_len = sizeof(struct sockaddr_in);
+
+	/* Bind */
+	if ((ret = bind(server_sockfd, 
+			(struct sockaddr *)&server_address, server_len)) < 0) {
+		printf("BIND ERROR. %d ret\n", ret);
+		exit(-1);
+	}
+	printf(" |-> Server Bind Succeed.\n");
+
+	/* listen */
+	while (1) {
+		memset(recvbuf, 0, sizeof(recvbuf));
+		client_len = recvfrom(server_sockfd, recvbuf, sizeof(recvbuf),
+				0, (struct sockaddr *)&client_address, &server_len);
+		if (client_len) {
+			printf(" |-> Server-Resv: %s\n", recvbuf);
+			sendto(server_sockfd, recvbuf, server_len, 0,
+				 (struct sockaddr *)&client_address, server_len);
+		}
+	}
+
+	close(server_sockfd);
+	close(client_sockfd);
+	return 0;
+}
