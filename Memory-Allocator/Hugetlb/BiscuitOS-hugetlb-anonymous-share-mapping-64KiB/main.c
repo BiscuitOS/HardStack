4a05e30584e6da4e321aafd10f514cea8916fd86
BuddyZhang1 <buddy.d.zhang@gmail.com>
2021-12-05 13:47:30 +0800

    update hugetlb 64K

--- Memory-Allocator/Hugetlb/BiscuitOS-hugetlb-anonymous-share-mapping-64KiB/main.c
+++ Memory-Allocator/Hugetlb/BiscuitOS-hugetlb-anonymous-share-mapping-64KiB/main.c
@@ -1,14 +1,14 @@
 /*
  * Hugetlb: Anonymous Shared-mapping for 64K Hugepage
  *
- * (C) 2021.11.11 BuddyZhang1 <buddy.zhang@aliyun.com>
+ * (C) 2021.12.05 BuddyZhang1 <buddy.zhang@aliyun.com>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 as
  * published by the Free Software Foundation.
  */
 #ifndef __aarch64__
-#error "Process Need running on ARM64"
+#error "Process Need running on ARM64 Architecture"
 #endif
 #include <stdio.h>
 #include <stdlib.h>
@@ -19,8 +19,13 @@
 #include <sys/mman.h>
 #include <linux/mman.h>
 
-#define HPAGE_SIZE		(2 * 1024 * 1024)
+#define HPAGE_SIZE		(64 * 1024)
 #define BISCUITOS_MAP_SIZE	(2 * HPAGE_SIZE)
+
+#ifdef __aarch64__
+#define HUGETLB_FLAG_ENCODE_SHIFT	26
+#define MAP_HUGE_64KB			(16 << HUGETLB_FLAG_ENCODE_SHIFT)
+#endif
 
 int main()
 {
@@ -42,6 +47,9 @@ int main()
 	/* Trigger page fault */
 	base[0] = 'B';
 	printf("%#lx => %c\n", (unsigned long)base, base[0]);
+
+	/* Just for debug */
+	sleep(-1);
 
 	/* unmap */
 	munmap(base, BISCUITOS_MAP_SIZE);
