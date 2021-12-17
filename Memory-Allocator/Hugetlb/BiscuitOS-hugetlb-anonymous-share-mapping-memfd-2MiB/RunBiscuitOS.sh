28cbe035f4c0a7f5a58df00f9e0deb15cfef5c4f
BuddyZhang1 <buddy.d.zhang@gmail.com>
2021-12-17 21:41:48 +0800

    hugetlb: memfd with 2MiB Hugepage

---
+++ Memory-Allocator/Hugetlb/BiscuitOS-hugetlb-anonymous-share-mapping-memfd-2MiB/RunBiscuitOS.sh
@@ -0,0 +1,19 @@
+#!/bin/ash
+# 
+
+echo 10 > /sys/kernel/mm/hugepages/hugepages-2048kB/nr_hugepages
+BiscuitOS-hugetlb-anonymous-share-mapping-memfd-2MiB-default-Server &
+sleep 1
+BiscuitOS-hugetlb-anonymous-share-mapping-memfd-2MiB-default-Client &
+sleep 1
+# Information for 2MiB hugepage pool
+nr_hugepage=$(cat /sys/kernel/mm/hugepages/hugepages-2048kB/nr_hugepages)
+free_hugepages=$(cat /sys/kernel/mm/hugepages/hugepages-2048kB/free_hugepages)
+resv_hugepages=$(cat /sys/kernel/mm/hugepages/hugepages-2048kB/resv_hugepages)
+surplus_hugepages=$(cat /sys/kernel/mm/hugepages/hugepages-2048kB/surplus_hugepages)
+echo "BiscuitOS 2MiB Hugepages"
+echo " HugePages_Total:  ${nr_hugepage}"
+echo " HugePages_Free:   ${free_hugepages}"
+echo " HugePages_Rsvd:   ${resv_hugepages}"
+echo " HugePages_Surp:   ${surplus_hugepages}"
+
