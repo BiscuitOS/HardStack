c24565a90c6d4ba505675795276761714d6be478
BuddyZhang1 <buddy.d.zhang@gmail.com>
2021-12-19 09:26:37 +0800

    hugetlb: populate with 2MiB hugepage

---
+++ Memory-Allocator/Hugetlb/BiscuitOS-hugetlb-anonymous-share-mapping-populate-2MiB/RunBiscuitOS.sh
@@ -0,0 +1,17 @@
+#!/bin/ash
+# 
+# Allocate 10 Hugepage to 2MiB hugepage pool
+echo 10 > /sys/kernel/mm/hugepages/hugepages-2048kB/nr_hugepages
+# Running program
+BiscuitOS-hugetlb-anonymous-share-mapping-populate-2MiB-default &
+sleep 1
+# Information for 2MiB hugepage pool
+nr_hugepage=$(cat /sys/kernel/mm/hugepages/hugepages-2048kB/nr_hugepages)
+free_hugepages=$(cat /sys/kernel/mm/hugepages/hugepages-2048kB/free_hugepages)
+resv_hugepages=$(cat /sys/kernel/mm/hugepages/hugepages-2048kB/resv_hugepages)
+surplus_hugepages=$(cat /sys/kernel/mm/hugepages/hugepages-2048kB/surplus_hugepages)
+echo "BiscuitOS 2MiB Hugepages"
+echo " HugePages_Total:  ${nr_hugepage}"
+echo " HugePages_Free:   ${free_hugepages}"
+echo " HugePages_Rsvd:   ${resv_hugepages}"
+echo " HugePages_Surp:   ${surplus_hugepages}"
