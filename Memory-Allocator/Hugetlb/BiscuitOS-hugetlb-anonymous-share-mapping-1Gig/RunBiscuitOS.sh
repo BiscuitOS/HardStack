674569ba1bc37fac6092bba7b87aec30449b0af2
BuddyZhang1 <buddy.d.zhang@gmail.com>
2021-12-21 06:42:04 +0800

    hugetlb: Support 1Gig Hugepage

---
+++ Memory-Allocator/Hugetlb/BiscuitOS-hugetlb-anonymous-share-mapping-1Gig/RunBiscuitOS.sh
@@ -0,0 +1,19 @@
+#!/bin/ash
+# 
+# Allocate 1 Hugepage to 1Gig hugepage pool
+echo 1 > /sys/kernel/mm/hugepages/hugepages-1048576kB/nr_hugepages
+# Running program
+BiscuitOS-hugetlb-anonymous-share-mapping-1Gig-default &
+sleep 1
+# Information for 1Gig hugepage pool
+nr_hugepage=$(cat /sys/kernel/mm/hugepages/hugepages-1048576kB/nr_hugepages)
+free_hugepages=$(cat /sys/kernel/mm/hugepages/hugepages-1048576kB/free_hugepages)
+resv_hugepages=$(cat /sys/kernel/mm/hugepages/hugepages-1048576kB/resv_hugepages)
+surplus_hugepages=$(cat /sys/kernel/mm/hugepages/hugepages-1048576kB/surplus_hugepages)
+echo "BiscuitOS 1Gig Hugepages"
+echo " HugePages_Total:  ${nr_hugepage}"
+echo " HugePages_Free:   ${free_hugepages}"
+echo " HugePages_Rsvd:   ${resv_hugepages}"
+echo " HugePages_Surp:   ${surplus_hugepages}"
+
+
---
+++ Memory-Allocator/Hugetlb/BiscuitOS-hugetlb-anonymous-share-mapping-1Gig/main.c
@@ -0,0 +1,58 @@
+/*
+ * Hugetlb: Anonymous Shared-mapping for 1Gig Hugepage
+ *
+ * (C) 2021.12.05 BuddyZhang1 <buddy.zhang@aliyun.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ */
+#ifdef __i386__
+#error "Process doesn't support I386 Architecture"
+#endif
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <sys/mman.h>
+#include <linux/mman.h>
+
+#define HPAGE_SIZE		(1 * 1024 * 1024 * 1024)
+#define BISCUITOS_MAP_SIZE	(1 * HPAGE_SIZE)
+
+#ifndef MAP_HUGE_1GB
+#define HUGETLB_FLAG_ENCODE_SHIFT	26
+#define MAP_HUGE_1GB			(30 << HUGETLB_FLAG_ENCODE_SHIFT)
+#endif
+
+int main()
+{
+	char *base;
+
+	/* mmap */
+	base = (char *)mmap(NULL, 
+			    BISCUITOS_MAP_SIZE,
+			    PROT_READ | PROT_WRITE,
+			    MAP_SHARED | MAP_ANONYMOUS | 
+			    MAP_HUGETLB | MAP_HUGE_1GB,
+			    -1,
+			    0);
+	if (base == MAP_FAILED) {
+		printf("ERROR: mmap failed.\n");
+		return -ENOMEM;
+	}
+
+	/* Trigger page fault */
+	base[0] = 'B';
+	printf("%#lx => %c\n", (unsigned long)base, base[0]);
+
+	/* Just for debug */
+	sleep(-1);
+
+	/* unmap */
+	munmap(base, BISCUITOS_MAP_SIZE);
+
+	return 0;
+}
