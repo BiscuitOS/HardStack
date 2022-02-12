34f35bc06e7aea2536848b0b67e577f736ebb93c
BuddyZhang1 <buddy.d.zhang@gmail.com>
2022-02-12 19:57:05 +0800

    interval tree on 4.9

---
+++ Algorithem/tree/interval-tree/BiscuitOS-file-interval-tree-4.9/main.c
@@ -0,0 +1,163 @@
+/*
+ * Fork usage for file interval tree on BiscuitOS
+ *
+ * (C) 2021.02.11 BuddyZhang1 <buddy.zhang@aliyun.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ */
+
+#include <linux/init.h>
+#include <linux/kernel.h>
+#include <linux/module.h>
+#include <linux/slab.h>
+#include <linux/fs.h>
+#include <linux/mm.h>
+#include "file_interval_tree.h"
+
+#define VADDR			0x60000000
+#define PAGE_NR			4
+
+/* Module initialize entry */
+static int __init BiscuitOS_init(void)
+{
+	struct address_space *mapping, *tmapping;
+	struct page *pages[PAGE_NR], *page;
+	struct vm_area_struct *vma, *tvma;
+	struct vm_area_struct *fvma0, *fvma1;
+	unsigned long start, end;
+	struct inode *inode;
+	struct file *file;
+	int ret = 0, i;
+
+	/* pseudo inode and file */
+	inode = kzalloc(sizeof(*inode), GFP_KERNEL);
+	if (!inode) {
+		printk("inode create failed.\n");
+		ret = -ENOMEM;
+		goto err;
+	}
+	inode->i_mapping = &inode->i_data;
+
+	file = kzalloc(sizeof(*file), GFP_KERNEL);
+	if (!file) {
+		printk("File create failed.\n");
+		ret = -ENOMEM;
+		goto err_file;
+	}
+	file->f_inode   = inode;
+	file->f_mapping = inode->i_mapping;
+
+	/*
+	 * VMA contain 4 memory regions and mapping 4 physical page
+	 *
+	 *   1st region: 0x600000000000 - 0x600000001000 --> 1st page
+	 *   2nd region: 0x600000001000 - 0x600000002000 --> 2nd page
+	 *   3rd region: 0x600000002000 - 0x600000003000 --> 3rd page
+	 *   4th region: 0x600000003000 - 0x600000004000 --> 4th page
+	 */
+	vma = kzalloc(sizeof(*vma), GFP_KERNEL);
+	if (!vma) {
+		printk("VMA create failed.\n");
+		ret = -ENOMEM;
+		goto err_vma;
+	}
+	/* mapping from head of file */
+	vma->vm_pgoff = 0;
+	vma->vm_start = VADDR;
+	vma->vm_end   = VADDR + PAGE_NR * PAGE_SIZE;
+	vma->vm_file  = get_file(file);
+	printk("Proccess VMA: %#lx\n", (unsigned long)vma);
+
+	/* insert into interval tree */
+	mapping = vma->vm_file->f_mapping;
+	BiscuitOS_vma_interval_tree_insert(vma, &mapping->i_mmap);
+
+	/* Emulate mapping physical page */
+	for (i = 0; i < PAGE_NR; i++) {
+		unsigned long vaddr = i * PAGE_SIZE + VADDR;
+		unsigned long index =
+			((vaddr - vma->vm_start) >> PAGE_SHIFT) + vma->vm_pgoff;
+
+		/* Assume alloc page success */
+		pages[i] = alloc_page(GFP_KERNEL);
+		/* rmap */
+		pages[i]->mapping = mapping;
+		pages[i]->index   = index;
+		printk("Page %#lx index %#lx\n",
+				page_to_pfn(pages[i]), pages[i]->index);
+	}
+
+	/* Emulate fork Child Process */
+	fvma0 = kzalloc(sizeof(*fvma0), GFP_KERNEL);
+	if (!fvma0) {
+		printk("Fork Child VMA create failed.\n");
+		ret = -ENOMEM;
+		goto err_fork0;
+	}
+	printk("Proccess VMA: %#lx\n", (unsigned long)fvma0);
+	*fvma0 = *vma;
+	BiscuitOS_vma_interval_tree_insert_after(fvma0, vma, &mapping->i_mmap);
+
+	/* Emulate fork Child Child Process0 */
+	fvma1 = kzalloc(sizeof(*fvma1), GFP_KERNEL);
+	if (!fvma1) {
+		printk("Fork Child Child VMA create failed.\n");
+		ret = -ENOMEM;
+		goto err_fork1;
+	}
+	printk("Proccess VMA: %#lx\n", (unsigned long)fvma1);
+	*fvma1 = *fvma0;
+	BiscuitOS_vma_interval_tree_insert_after(fvma1, vma, &mapping->i_mmap);
+
+	/* Traversal interal tree for special page */
+	page  = pages[2];
+	tmapping = (void *)((unsigned long)page->mapping & ~PAGE_MAPPING_FLAGS);
+
+	start = page->index;
+	end   = start + 1;
+	BiscuitOS_vma_interval_tree_foreach(tvma, 
+					&tmapping->i_mmap, start, end) {
+		unsigned long address =
+			((page->index - tvma->vm_pgoff) << PAGE_SHIFT) +
+							tvma->vm_start;
+
+		printk("Page %#lx with vaddr: %#lx and "
+				"VMA [%#lx] %#lx - %#lx\n",
+			page_to_pfn(page), address,
+			(unsigned long)tvma, tvma->vm_start, tvma->vm_end);
+	}
+
+	/* remove */
+	BiscuitOS_vma_interval_tree_remove(fvma1, &mapping->i_mmap);
+	BiscuitOS_vma_interval_tree_remove(fvma0, &mapping->i_mmap);
+	BiscuitOS_vma_interval_tree_remove(vma, &mapping->i_mmap);
+
+	for (i = 0; i < PAGE_NR; i++)
+		__free_page(pages[i]);
+
+	kfree(fvma1);
+err_fork1:
+	kfree(fvma0);
+err_fork0:
+	kfree(vma);
+err_vma:
+	kfree(file);
+err_file:
+	kfree(inode);
+err:
+	return ret;
+}
+
+/* Module exit entry */
+static void __exit BiscuitOS_exit(void)
+{
+}
+
+module_init(BiscuitOS_init);
+module_exit(BiscuitOS_exit);
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("BiscuitOS <buddy.zhang@aliyun.com>");
+MODULE_DESCRIPTION("File interval tree on BiscuitOS");
