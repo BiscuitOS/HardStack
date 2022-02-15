0eec575467576e66759c4d0bf48896f5a3b13340
BuddyZhang1 <buddy.d.zhang@gmail.com>
2022-02-15 10:11:02 +0800

    fix bug

--- SystemCall/sys_hello_BiscuitOS/debug_BiscuitOS_common/BiscuitOS_debug.h
+++ SystemCall/sys_hello_BiscuitOS/debug_BiscuitOS_common/BiscuitOS_debug.h
@@ -7,18 +7,19 @@
         extern int bs_debug_enable;                             \
         if (bs_debug_enable)                                    \
                 pr_info(__VA_ARGS__);                           \
-})                                                              \
-                                                                \
+})
+
 #define bs_debug_enable()                                       \
 ({                                                              \
         extern int bs_debug_enable;                             \
         bs_debug_enable = 1;                                    \
 })                                                              \
-                                                                \
+
 #define bs_debug_disable()                                      \
 ({                                                              \
         extern int bs_debug_enable;                             \
         bs_debug_enable = 0;                                    \
 })
+
 
 #endif
