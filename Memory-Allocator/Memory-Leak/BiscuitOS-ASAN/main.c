11462c7646632640ff369b0c5d0b2b0e5e9f8d5a
BuddyZhang1 <buddy.d.zhang@gmail.com>
2022-02-28 13:04:49 +0800

    MemoryLeak: GCC ASAN

---
+++ Memory-Allocator/Memory-Leak/BiscuitOS-ASAN/main.c
@@ -0,0 +1,21 @@
+/*
+ * ASAN: Out-of-bounds access to stack
+ *
+ * (C) 2022.02.28 BuddyZhang1 <buddy.zhang@aliyun.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+int main()
+{
+	int array[10];
+
+	array[10] = 0x20;
+	return 0;
+}
