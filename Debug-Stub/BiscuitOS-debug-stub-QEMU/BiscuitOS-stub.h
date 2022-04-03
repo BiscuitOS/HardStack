5263f12d03981e44412b6c74e0f925bf1f010986
BuddyZhang1 <buddy.d.zhang@gmail.com>
2022-04-03 20:58:06 +0800

    updage

--- Debug-Stub/BiscuitOS-debug-stub-QEMU/BiscuitOS-stub.h
+++ Debug-Stub/BiscuitOS-debug-stub-QEMU/BiscuitOS-stub.h
@@ -1,22 +1,21 @@
 #ifndef _BISCUITOS_STUB_H
 #define _BISCUITOS_STUB_H
 
+extern int bs_debug_enable_qemu;
+
 #define bs_debug(...)                       \
 ({                                          \
-    extern int bs_debug_enable_qemu;        \
     if (bs_debug_enable_qemu)               \
         printf(__VA_ARGS__);                \
 })
 
 #define bs_debug_enable()                   \
 ({                                          \
-    extern int bs_debug_enable_qemu;        \
     bs_debug_enable_qemu = 1;               \
 })
 
 #define bs_debug_disable()                  \
 ({                                          \
-    extern int bs_debug_enable_qemu;        \
     bs_debug_enable_qemu = 0;               \
 })
 
