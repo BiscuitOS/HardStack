fc32105f13c7360ad195d69b16c299b87aa5a7d6
BuddyZhang1 <buddy.d.zhang@gmail.com>
2022-04-25 08:29:11 +0800

    sync

--- Memory-Allocator/THP/BiscuitOS-THP-userspace/README.md
+++ Memory-Allocator/THP/BiscuitOS-THP-userspace/README.md
@@ -4,3 +4,9 @@ THP on userpsace
 #### Prepare
 
 we need enable macro "CONFIG_TRANSPARENT_HUGEPAGE" and "CONFIG_TRANSPARENT_HUGEPAGE_ALWAYS", and total RAM is must bigger then 512MiB!
+
+Then, we need enable shmem_huge, e.g.
+
+```
+echo always > /sys/kernel/mm/transparent_hugepage/shmem_enabled
+``
--- Memory-Allocator/THP/BiscuitOS-THP-userspace/RunBiscuitOS.sh
+++ Memory-Allocator/THP/BiscuitOS-THP-userspace/RunBiscuitOS.sh
@@ -1,5 +1,10 @@
 #!/bin/ash
 # 
+# Remount
+# umount /tmp/
+# mount -t tmpfs -o huge=always nodev /tmp/
+# Enable shmem huge
+echo always > /sys/kernel/mm/transparent_hugepage/shmem_enabled
 # Setup scan sleep
 echo 3000 > /sys/kernel/mm/transparent_hugepage/khugepaged/scan_sleep_millisecs
 # Running program
