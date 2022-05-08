61c0f44ea80ef4f2c8feffaf51e1f227aa83d4e7
BuddyZhang1 <buddy.d.zhang@gmail.com>
2022-05-08 09:13:50 +0800

    s

---
+++ Memory-Allocator/THP/BiscuitOS-THP-ShmemHugepage-file-shared/RunBiscuitOS.sh
@@ -0,0 +1,7 @@
+#!/bin/ash
+# 
+# Mount huge tmpfs
+mkdir -p /BiscuitOS-tmpfs/
+mount -t tmpfs nodev -o huge=always /BiscuitOS-tmpfs/
+# Running program
+BiscuitOS-THP-ShmemHugepage-file-shred-default &
