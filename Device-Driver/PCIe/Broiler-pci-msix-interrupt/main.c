fbd75476f674d74dd444b3b236c2eb09aac79012
BuddyZhang1 <buddy.d.zhang@gmail.com>
2022-08-09 19:23:03 +0800

    Broiler: MSIX Interrupt

---
+++ Device-Driver/PCIe/Broiler-pci-msix-interrupt/main.c
@@ -0,0 +1,158 @@
+/*
+ * Broiler PCI MSIX Interrupt
+ *
+ * (C) 2022.08.09 BuddyZhang1 <buddy.zhang@aliyun.com>
+ * (C) 2022.07.22 BiscuitOS <https://biscuitos.github.io/>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ */
+
+#include <linux/init.h>
+#include <linux/kernel.h>
+#include <linux/module.h>
+#include <linux/miscdevice.h>
+#include <linux/fs.h>
+#include <linux/slab.h>
+#include <linux/uaccess.h>
+#include <linux/pci.h>
+
+#define DEV_NAME		"Broiler-PCI-MSIX"
+#define IO_BAR			0x00
+
+/* Doorball Register */
+#define DOORBALL_REG		0x10
+
+struct Broiler_pci_device {
+	struct pci_dev *pdev;
+	/* IO BAR */
+	void __iomem *io;
+};
+static struct Broiler_pci_device *bpdev;
+
+static irqreturn_t Broiler_msix_handler(int irq, void *dev)
+{
+	/* TODO */
+	printk("%s Receive MSIX Interrupt %d\n", DEV_NAME, irq);
+	return IRQ_HANDLED;
+}
+
+static int Broiler_pci_probe(struct pci_dev *pdev, 
+				const struct pci_device_id *id)
+{
+	struct msix_entry msix;
+	int r = 0;
+
+	bpdev = kzalloc(sizeof(*bpdev), GFP_KERNEL);
+	if (!bpdev) {
+		r = -ENOMEM;
+		printk("%s ERROR: Broiler PCI allocate failed.\n", DEV_NAME);
+		goto err_alloc;
+	}
+	bpdev->pdev = pdev;
+
+	/* Enable PCI device */
+	r = pci_enable_device(pdev);
+	if (r < 0) {
+		printk("%s ERROR: PCI Device Enable failed.\n", DEV_NAME);
+		goto err_enable_pci;
+	}
+
+	/* Request IO BAR resource */
+	r = pci_request_region(pdev, IO_BAR, "Broiler-PCIe-INTX-IO");
+	if (r < 0) {
+		printk("%s ERROR: Request IO-BAR Failed.\n", DEV_NAME);
+		goto err_request_io;
+	}
+
+	/* Remapping IO-BAR */
+	bpdev->io = pci_iomap(pdev, IO_BAR, pci_resource_len(pdev, IO_BAR));
+	if (!bpdev->io) {
+		r = -EBUSY;
+		printk("%s ERROR: Remapping IO Failed\n", DEV_NAME);
+		goto err_iomap;
+	}
+
+	/* Reqeust MSIX Interrupt */
+	msix = (struct msix_entry) {
+		.vector = 0,
+		.entry  = 0, /* 1st msix interrupt */
+	};
+	r = pci_enable_msix_range(pdev, &msix, 1, 1);
+	if (r != 1) { /* Only request 1 MSIX Interrupt */
+		printk("%s ERROR: Enable MSIX failed.\n", DEV_NAME);
+		goto err_msix;
+	}
+
+	r = request_irq(msix.vector, Broiler_msix_handler,
+				IRQF_SHARED | IRQF_TRIGGER_HIGH,
+						DEV_NAME, (void *)bpdev);
+	if (r < 0) {
+		printk("%s ERROR: Request IRQ failed.\n", DEV_NAME);
+		goto err_irq;
+	}
+
+	/* Set master */
+	pci_set_master(pdev);
+
+	printk("%s Success Register PCIe Device.\n", DEV_NAME);
+
+	/* Doorball: kick off, trigger MSIX interrupt  */
+	iowrite16(0x1, bpdev->io + DOORBALL_REG);
+
+	return 0;
+
+err_irq:
+	pci_disable_msix(pdev);
+err_msix:
+	pci_iounmap(pdev, bpdev->io);
+err_iomap:
+	pci_release_region(pdev, IO_BAR);
+err_request_io:
+	pci_disable_device(pdev);
+err_enable_pci:
+	kfree(bpdev);
+	bpdev = NULL;
+err_alloc:
+	return r;
+}
+
+static void Broiler_pci_remove(struct pci_dev *pdev)
+{
+	free_irq(pdev->irq, NULL);
+	pci_disable_msix(pdev);
+	pci_iounmap(pdev, bpdev->io);
+	pci_release_region(pdev, IO_BAR);
+	pci_disable_device(pdev);
+	kfree(bpdev);
+	bpdev = NULL;
+}
+
+static const struct pci_device_id Broiler_pci_ids[] = {
+	{ PCI_DEVICE(0x1025, 0x1991), },
+};
+
+static struct pci_driver Broiler_PCIe_driver = {
+	.name		= DEV_NAME,
+	.id_table	= Broiler_pci_ids,
+	.probe		= Broiler_pci_probe,
+	.remove		= Broiler_pci_remove,
+};
+
+static int __init Broiler_init(void)
+{
+	return pci_register_driver(&Broiler_PCIe_driver);
+}
+
+static void __exit Broiler_exit(void)
+{
+	pci_unregister_driver(&Broiler_PCIe_driver);
+}
+
+module_init(Broiler_init);
+module_exit(Broiler_exit);
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("BiscuitOS <buddy.zhang@aliyun.com>");
+MODULE_DESCRIPTION("Broiler PCI MSIX Interrupt");
