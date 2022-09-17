531a60f1a52f1485b5b99cc11d121719b4a30d16
BuddyZhang1 <buddy.d.zhang@gmail.com>
2022-09-17 15:39:29 +0800

    sync

--- Memory-Allocator/PIO/Broiler-Synchronous-pio/main.c
+++ Memory-Allocator/PIO/Broiler-Synchronous-pio/main.c
@@ -1,5 +1,5 @@
 /*
- * Broiler PIO on BiscuitOS
+ * Broiler Synchronous PIO on BiscuitOS
  *
  * (C) 2022.08.01 BuddyZhang1 <buddy.zhang@aliyun.com>
  *
@@ -14,7 +14,7 @@
 #include <linux/ioport.h>
 #include <linux/io.h>
 
-#define BISCUITOS_PIO_PORT	0x6800
+#define BISCUITOS_PIO_PORT	0x6060
 #define BISCUITOS_PIO_LEN	0x10
 #define SLOT_NUM_REG		0x00
 #define SLOT_SEL_REG		0x04
@@ -22,7 +22,7 @@
 #define MAX_FREQ_REG		0x0C
 
 static struct resource Broiler_pio_res = {
-	.name	= "Broiler PIO",
+	.name	= "Broiler Synchronous PIO",
 	.start	= BISCUITOS_PIO_PORT,
 	.end	= BISCUITOS_PIO_PORT + BISCUITOS_PIO_LEN,
 	.flags	= IORESOURCE_IO,
@@ -71,4 +71,4 @@ module_exit(BiscuitOS_exit);
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("BiscuitOS <buddy.zhang@aliyun.com>");
-MODULE_DESCRIPTION("Broiler PIO on BiscuitOS");
+MODULE_DESCRIPTION("Broiler Synchronous PIO on BiscuitOS");
