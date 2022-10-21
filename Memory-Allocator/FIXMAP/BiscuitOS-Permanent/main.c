10e9a754fa0f541ba29b23339a733760e9a7a1e7
BuddyZhang1 <buddy.d.zhang@gmail.com>
2022-10-21 08:21:04 +0800

    s

---
+++ Memory-Allocator/FIXMAP/BiscuitOS-Permanent/main.c
@@ -0,0 +1,30 @@
+/*
+ * Permanent Mapping Memory Allocator: add new entry
+ *
+ * (C) 2022.10.16 BuddyZhang1 <buddy.zhang@aliyun.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ */
+#include <linux/init.h>
+#include <linux/kernel.h>
+#include <asm/fixmap.h>
+
+#define BROILER_MMIO_BASE	0xD0000000
+
+/* kernel entry on initcall */
+static int __init BiscuitOS_init(void)
+{
+	unsigned long addr;
+
+	set_fixmap_io(FIX_BISCUITOS, BROILER_MMIO_BASE);
+	addr = fix_to_virt(FIX_BISCUITOS);
+
+	printk("Broiler MMIO Read %#lx: %#x", addr, *(unsigned int *)addr);
+
+	clear_fixmap(FIX_BISCUITOS);
+	return 0;
+}
+
+device_initcall(BiscuitOS_init);
