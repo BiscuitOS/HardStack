71032b10ccab86e5791710eba24c8e9895c1fec2
BuddyZhang1 <buddy.d.zhang@gmail.com>
2022-11-13 09:03:02 +0800

    sync

---
+++ Memory-Allocator/MEMBLOCK/BiscuitOS-MEMBLOCK-memblock_alloc_node/main.c
@@ -0,0 +1,47 @@
+/*
+ * MEMBLOCK Memory Allocator: memblock_alloc_node
+ *
+ * (C) 2022.10.16 BuddyZhang1 <buddy.zhang@aliyun.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ */
+#include <linux/init.h>
+#include <linux/kernel.h>
+#include <linux/memblock.h>
+
+/* BiscuitOS NUMA Layout
+ *  - Layout0: 2CPUs with 2 NUMA NODE
+ *    NODE0: 0x0000000 - 0x10000000
+ *    NODE1: 0x1000000 - 0x20000000
+ *
+ *  - Layout1: 4CPUs with 3 NUMA NODE
+ *    NODE0: 0x0000000 - 0x20000000
+ *    NODE1: 0x2000000 - 0x30000000
+ *    NODE2: 0x3000000 - 0x40000000
+ */
+#define MEMBLOCK_FAKE_SIZE	0x10
+#define MEMBLOCK_FAKE_NODE	1
+
+int __init BiscuitOS_Running(void)
+{
+	void *mem;
+
+	/* Alloc Memory */
+	mem = memblock_alloc_node(MEMBLOCK_FAKE_SIZE,
+				SMP_CACHE_BYTES, MEMBLOCK_FAKE_NODE);
+	if (!mem) {
+		printk("FATAL ERROR: No Free Memory on MEMBLOCK.\n");
+		return -ENOMEM;
+	}
+
+	sprintf(mem, "Hello %s", "BiscuitOS");
+	printk("==== %s ====\n", (char *)mem);
+	printk("Physical Address %#lx\n", __pa(mem));
+
+	/* Free Memory */
+	memblock_free(mem, MEMBLOCK_FAKE_SIZE);
+
+	return 0;
+}
