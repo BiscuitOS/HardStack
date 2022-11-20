98562c4ae2f9946795632afe30e5c5fd79f4078b
BuddyZhang1 <buddy.d.zhang@gmail.com>
2022-11-21 07:46:16 +0800

    sync

--- Memory-Allocator/MEMBLOCK/BiscuitOS-MEMBLOCK-memblock_add_node/main.c
+++ Memory-Allocator/MEMBLOCK/BiscuitOS-MEMBLOCK-memblock_add_node/main.c
@@ -11,14 +11,34 @@
 #include <linux/kernel.h>
 #include <linux/memblock.h>
 
+/* BiscuitOS NUMA Layout
+ *  - Layout0: 2CPUs with 2 NUMA NODE
+ *    NODE0: 0x00000000 - 0x100000000
+ *    NODE1: 0x10000000 - 0x200000000
+ *
+ *  - Layout1: 4CPUs with 3 NUMA NODE
+ *    NODE0: 0x00000000 - 0x200000000
+ *    NODE1: 0x20000000 - 0x300000000
+ *    NODE2: 0x30000000 - 0x400000000
+ */
 #define MEMBLOCK_FAKE_BASE	0x800000000
 #define MEMBLOCK_FAKE_SIZE	0x100000
+#define MEMBLOCK_FAKE_NODE	1
 
 int __init BiscuitOS_Running(void)
 {
+	phys_addr_t region_start, region_end;
+	u64 idx;
+	int nid;
+
 	/* Add new range */
 	memblock_add_node(MEMBLOCK_FAKE_BASE, MEMBLOCK_FAKE_SIZE,
-					NUMA_NO_NODE, MEMBLOCK_NONE);
+					MEMBLOCK_FAKE_NODE, MEMBLOCK_NONE);
+
+	__for_each_mem_range(idx, &memblock.memory, NULL, MEMBLOCK_FAKE_NODE,
+				MEMBLOCK_NONE, &region_start, &region_end, &nid)
+		printk("Region %llx: %#llx - %#llx NID %d\n", idx,
+					region_start, region_end, nid);
 
 	/* Only Test: Remove range */
 	memblock_remove(MEMBLOCK_FAKE_BASE, MEMBLOCK_FAKE_SIZE);
