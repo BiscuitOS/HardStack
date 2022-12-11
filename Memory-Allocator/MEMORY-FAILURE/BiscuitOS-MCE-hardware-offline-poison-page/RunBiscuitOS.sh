cf833f930821dd75d0ea31e7d1334ba9047ab6ca
BuddyZhang1 <buddy.d.zhang@gmail.com>
2022-12-11 13:44:52 +0800

    sync

--- Memory-Allocator/MEMORY-FAILURE/BiscuitOS-MCE-hardware-offline-poison-page/RunBiscuitOS.sh
+++ Memory-Allocator/MEMORY-FAILURE/BiscuitOS-MCE-hardware-offline-poison-page/RunBiscuitOS.sh
@@ -7,5 +7,9 @@ sleep 0.2
 POISON_PADDR=$(cat /tmp/.posion_paddr.txt)
 # Hardware offline poison page
 [ ! -f /sys/devices/system/memory/hard_offline_page ] && echo "Pls Enable CONFIG_MEMORY_FAILURE" && exit 1
+
+# Just for debug
+# echo 1 > /proc/sys/BiscuitOS/bs_debug_enable
 echo ${POISON_PADDR} > /sys/devices/system/memory/hard_offline_page
+# echo 0 > /proc/sys/BiscuitOS/bs_debug_enable
 echo "Offline Poison Page ${POISON_PADDR}"
