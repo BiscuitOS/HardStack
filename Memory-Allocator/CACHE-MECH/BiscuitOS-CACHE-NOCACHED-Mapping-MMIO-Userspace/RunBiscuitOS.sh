8b65dd095a0b7b351b7140a5b1ac55546727217a
BuddyZhang1 <buddy.d.zhang@gmail.com>
2023-02-07 07:44:26 +0800

    sy

--- Memory-Allocator/CACHE-MECH/BiscuitOS-CACHE-NOCACHED-Mapping-MMIO-Userspace/RunBiscuitOS.sh
+++ Memory-Allocator/CACHE-MECH/BiscuitOS-CACHE-NOCACHED-Mapping-MMIO-Userspace/RunBiscuitOS.sh
@@ -1,4 +1,4 @@
 #!/bin/ash
-insmod /lib/modules/$(uname -r)/extra/BiscuitOS-CACHE-NOCACHE--mapping-MMIO-Userspace-default.ko
+insmod /lib/modules/$(uname -r)/extra/BiscuitOS-CACHE-NOCACHED-Mapping-MMIO-Userspace-default.ko
 APP &
 cat /sys/kernel/debug/x86/pat_memtype_list
