19ac7e0172fe96467c247cb7a68d6ef6b7975688
buddy.zhang <buddy.zhang@biscuitos.cn>
2023-05-04 19:48:58 +0800

    CACHE COLOURI

---
+++ Memory-Allocator/CACHE-MECH/BiscuitOS-CACHE-COLOURING/RunBiscuitOS.sh
@@ -0,0 +1,9 @@
+#!/bin/ash
+# 
+# Allocate 1 Hugepage to 1Gig hugepage pool
+echo 1 > /sys/kernel/mm/hugepages/hugepages-1048576kB/nr_hugepages
+sleep 0.1
+# Running program
+BiscuitOS-CACHE-COLOURING-default &
+
+
